a14a2fa78a47317a45bace91af5699e2423de3bc
Michael Buckley <michael.buckley@intelliware.com>
2014-05-23 13:37:26 -0400

    Start web api

--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistrationController.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistrationController.java
@@ -3,16 +3,25 @@ package ca.intelliware.ihtsdo.mlds.registration;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserRegistrationController {
 	@Resource
 	UserRegistrationService userRegistrationService;
+	
+	@Resource
+	UserRegistrationRepository userRegistrationRepository;
 
+	@RequestMapping("/registrations/create")
 	public void createRegistration(String email) {
 		userRegistrationService.createRegistration(email);
 	}
-	
-	
+
+	@RequestMapping("/registrations")
+	public @ResponseBody Iterable<UserRegistration> getRegistrations() {
+		return userRegistrationRepository.findAll();
+	}
 
 }
