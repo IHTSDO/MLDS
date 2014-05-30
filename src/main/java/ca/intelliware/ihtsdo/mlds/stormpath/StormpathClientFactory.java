17b963affffd34565ce572905044a3c2c5ee45fe
Michael Buckley <michael.buckley@intelliware.com>
2014-05-30 16:10:26 -0400

    Delay web package scanning until dispatch servlet context

--- src/main/java/ca/intelliware/ihtsdo/mlds/stormpath/StormpathClientFactory.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/stormpath/StormpathClientFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ca.intelliware.ihtsdo.mlds.web;
+package ca.intelliware.ihtsdo.mlds.stormpath;
 
 import java.io.InputStream;
 import java.io.Reader;
