9e947399432ae5b6ceb9f9fe619028499fe57a3d
Michael Buckley <michael.buckley@intelliware.com>
2014-05-30 16:11:03 -0400

    Add registrations to admin dashboard

---
+++ src/main/webapp/js/app/controllers/AdminDashboardController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('MLDS')
+    .controller('AdminDashboardController',
+        [ '$scope', '$log', function ($scope, $log) {
+        	
+        	console.log('Dashboard Controller');
+        }
+    ]);
