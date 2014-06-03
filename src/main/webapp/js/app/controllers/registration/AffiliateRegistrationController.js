9b11caeb911ed199eaa2a89f0554f5219b98a94f
anson-chung <anson.chung@intelliware.com>
2014-06-03 14:23:44 -0400

    adding AffiliateRegistrationController.js

---
+++ src/main/webapp/js/app/controllers/registration/AffiliateRegistrationController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('MLDS')
+    .controller('AffiliateRegistrationController',
+        [ '$scope', '$log',  function ($scope, $log) {
+        	
+        	$scope.affilateform = {};
+        	
+        	$scope.collapsePanel = {};
+        	
+        }
+    ]);
