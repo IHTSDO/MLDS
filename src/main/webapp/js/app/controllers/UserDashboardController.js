fe5409fdc6b88ccb1883513232c279a999388456
Michelle Andrews <insomniacdeziner@gmail.com>
2014-06-03 12:50:24 -0400

    added footer.html

--- src/main/webapp/js/app/controllers/UserDashboardController.js
+++ src/main/webapp/js/app/controllers/UserDashboardController.js
@@ -3,8 +3,12 @@
 angular.module('MLDS')
     .controller('UserDashboardController',
         [ '$scope', '$log', '$location', function ($scope, $log, $location) {
+        	var isApplied = false;
         	var isApproved = false;
-        	if (!isApproved) {
+        	if (!isApplied) {
+        		$location.path('/affiliateRegistration');
+        		
+        	} else if (!isApproved) {
         		// redirect to pending approval
         		$location.path('/pendingRegistration');
         	}
