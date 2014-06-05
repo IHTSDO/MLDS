a7801c488fea0cd7f828afb5cbaecfa0b5105e65
anson-chung <anson.chung@intelliware.com>
2014-06-05 10:14:30 -0400

    Wiring up application form to database and displaying applications on admin page
    Conflicts:
    	src/main/webapp/js/app/templates/registration/affiliateRegistration.html

--- src/main/webapp/js/app/controllers/AdminDashboardController.js
+++ src/main/webapp/js/app/controllers/AdminDashboardController.js
@@ -2,8 +2,21 @@
 
 angular.module('MLDS')
     .controller('AdminDashboardController',
-        [ '$scope', '$log', function ($scope, $log) {
+        [ '$scope', '$log', 'UserRegistrationService', function ($scope, $log, UserRegistrationService) {
         	
         	console.log('Dashboard Controller');
+        	
+        	$scope.applications = {};
+        	
+        	
+        	function getApplications() {
+        		var queryPromise =  UserRegistrationService.getApplications();
+        		
+        		queryPromise.success(function(data) {
+        			$scope.applications = data;
+        		});
+        	}
+        	
+        	getApplications();
         }
     ]);
