d0d8fe1d1e9beb2b36b7a418ea945d071806ee35
anson-chung <anson.chung@intelliware.com>
2014-06-05 10:14:30 -0400

    Wiring up application form to database and displaying applications on admin page
    Conflicts:
    	src/main/webapp/js/app/templates/registration/affiliateRegistration.html

--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/Application.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/Application.java
@@ -16,6 +16,16 @@ public class Application {
 	
 	String username;
 	boolean approved;
+	String type;
+	String name;
+	String address;
+	String city;
+	String country;
+	String phonenumber;
+	String extension;
+	String position;
+	String website;
+	boolean snomedlicense;
 	
 	public String getUsername() {
 		return username;
@@ -32,6 +42,86 @@ public class Application {
 
 	public boolean isApproved() {
 		return approved;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPhoneNumber() {
+		return phonenumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phonenumber = phoneNumber;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public boolean isSnoMedLicence() {
+		return snomedlicense;
+	}
+
+	public void setSnoMedLicence(boolean snoMedLicence) {
+		this.snomedlicense = snoMedLicence;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
 	}
 
 }
