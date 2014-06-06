2eb01c28f285ad21a09d274b26d91277b85aa5b7
anson-chung <anson.chung@intelliware.com>
2014-06-06 10:46:41 -0400

    Fixing ie8 issues and removing unused files

--- src/main/webapp/js/app/controllers/UserRegistrationApprovalController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('MLDS')
-    .controller('UserRegistrationApprovalsController',
-        [ '$scope', '$log', function ($scope, $log) {
-        	
-        	$scope.status = "processing";
-        }
-    ]);
