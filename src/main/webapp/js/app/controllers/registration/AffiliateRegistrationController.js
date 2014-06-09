dc2bcf75903e3a19472b72fa3d6d8ce53f765a05
Michael Buckley <michael.buckley@intelliware.com>
2014-06-09 15:04:23 -0400

    Update placeholder copy to give flavour of required content Update application page to be more live

--- src/main/webapp/js/app/controllers/registration/AffiliateRegistrationController.js
+++ src/main/webapp/js/app/controllers/registration/AffiliateRegistrationController.js
@@ -4,8 +4,21 @@ angular.module('MLDS')
     .controller('AffiliateRegistrationController',
         [ '$scope', '$log', 'UserRegistrationService', '$location', 'UserSession', function ($scope, $log, UserRegistrationService, $location, UserSession) {
         	
+        	window.regScope = $scope;
         	$scope.affiliateform = {};
         	
+        	$scope.$watch('affiliateform.type', function(newValue, oldValue) {
+        		var firstTimeSet = newValue && !oldValue;
+        		if (firstTimeSet) {
+        			$scope.collapsePanel.type = true;
+        		}
+        	});
+        	$scope.$watch('affiliateform.applicantType', function(newValue, oldValue) {
+        		var firstTimeSet = newValue && !oldValue;
+        		if (firstTimeSet) {
+        			$scope.collapsePanel.applicantType = true;
+        		}
+        	});
         	$scope.affiliateform.submit = function affiliateRegistrationSubmit() {
 				$log.log('AffiliateRegistrationController submit()', $scope.affiliateform);
 
