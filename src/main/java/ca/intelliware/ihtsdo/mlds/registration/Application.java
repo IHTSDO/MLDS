b5b30caf1990d3aa74f15057bf534c137dde86c9
Michael Buckley <michael.buckley@intelliware.com>
2014-06-09 13:48:47 -0400

    Introduce hibernate NamingStrategy

--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/Application.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/Application.java
@@ -7,11 +7,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="application")
 public class Application {
 	@Id
 	@GeneratedValue
-	@Column(name="application_id")
     private Long applicationId;
 	
 	String username;
--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistration.java
@@ -1,17 +1,13 @@
 package ca.intelliware.ihtsdo.mlds.registration;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="user_registration")
 public class UserRegistration {
 	@Id
 	@GeneratedValue
-	@Column(name="user_registration_id")
     private Long userRegistrationId;
 
 	String email;
