6c6d8bebe24d4c9362463102d617a31c6c398ee7
anson-chung <anson.chung@intelliware.com>
2014-06-11 13:34:48 -0400

    moving bsValidation over and separating out directives

---
+++ src/main/webapp/scripts/directives/activeMenu.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('mldsApp')
+    .directive('activeMenu', ['$translate', '$locale', 'tmhDynamicLocale', function($translate, $locale, tmhDynamicLocale) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs, controller) {
+                var language = attrs.activeMenu;
+
+                scope.$watch(function() {
+                    return $translate.use();
+                }, function(selectedLanguage) {
+                    if (language === selectedLanguage) {
+                        tmhDynamicLocale.set(language);
+                        element.addClass('active');
+                    } else {
+                        element.removeClass('active');
+                    }
+                });
+            }
+        };
+    }]);
