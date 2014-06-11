a387c651feec939252137536f4e2d9aeab214c91
anson-chung <anson.chung@intelliware.com>
2014-06-11 11:59:13 -0400

    Add explicit @table and @column names to work around broken namingstrategy

--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/UserRegistration.java
@@ -1,12 +1,16 @@
 package ca.intelliware.ihtsdo.mlds.registration;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="user_registration")
 public class UserRegistration {
 	@Id
+	@Column(name="user_registration_id")
 	@GeneratedValue
     private Long userRegistrationId;
 
