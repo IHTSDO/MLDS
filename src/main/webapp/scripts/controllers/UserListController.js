c93dd6135534847788711c3d8db57fe9dbbddbc7
anson-chung <anson.chung@intelliware.com>
2014-06-11 11:29:14 -0400

    adding in admin dashboard views/admin/audits.html

---
+++ src/main/webapp/scripts/controllers/UserListController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+mldsApp.controller('UserListController',
+        [ '$scope', '$log', 'UserRegistrationService', 'Events', function ($scope, $log, UserRegistrationService, Events) {
+        	$scope.users = [];
+        	
+        	function getUsers() {
+        		var queryPromise =  UserRegistrationService.getUsers();
+        		
+        		queryPromise.success(function(data) {
+        			$scope.users = data;
+        		});
+        	}
+        	
+        	getUsers();
+        	
+        	$scope.$on(Events.newUser, getUsers);
+        }
+    ]);
