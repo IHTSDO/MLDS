eb04d0f32f1b7e9ff149645164b3651be1739971
Michael Buckley <michael.buckley@intelliware.com>
2014-06-19 13:58:21 -0400

    Replace placeholder countries with real service call

--- src/main/webapp/scripts/services/CountryService.js
+++ src/main/webapp/scripts/services/CountryService.js
@@ -1,38 +1,30 @@
 'use strict';
 
 mldsApp.factory('CountryService', ['$http', '$log', '$q', function($http, $log, $q){
-		return {
-			getCountries: function() {
-				return $q.when([
-					{
-						isoCode2: 'DK',
-						isoCode3: 'DNK',
-						commonName: 'Denmark'
-					},
-					{
-						isoCode2: 'FR',
-						isoCode3: 'FRA',
-						commonName: 'France'
-					},
-					{
-						isoCode2: 'UA',
-						isoCode3: 'URE',
-						commonName: 'United Arab Emirates'
-					},
-					{
-						isoCode2: 'GB',
-						isoCode3: 'GBP',
-						commonName: 'United Kingdom'
-					},
-					{
-						isoCode2: 'US',
-						isoCode3: 'USA',
-						commonName: 'United States'
-					}
-				]);
-				//FIXME retrieve countries from server
-				//return $http.get('/app/countries');
-			},
+	
+		var countriesListQ = $http.get('/app/rest/countries')
+			.then(function(d){return d.data;});
+		var service = {};
+		
+		service.countries = [];
+		service.countriesByCode = {};
+		
+		service.getCountries = function getCountries() {
+			return countriesListQ;
 		};
+		
+		countriesListQ.then(function(countries){
+			// append to countries list
+			Array.prototype.push.apply(service.countries,countries);
+			
+			// fill countriesByCode map
+			service.countries.map(function(c){
+				service.countriesByCode[c.isoCode2] = c;
+			});
+			
+			$log.log('CountryService', service);
+		});
+		
+		return service;
 		
 	}]);
