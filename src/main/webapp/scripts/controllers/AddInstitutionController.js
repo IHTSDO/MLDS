39fbde56847535a3f64c42f6f02f0189393e5fdb
anson-chung <anson.chung@intelliware.com>
2014-06-19 12:19:58 -0400

    adding in button for seeing alerts and showing spinner when request is submitting

--- src/main/webapp/scripts/controllers/AddInstitutionController.js
+++ src/main/webapp/scripts/controllers/AddInstitutionController.js
@@ -1,8 +1,32 @@
 'use strict';
 
-angular.module('MLDS').controller('AddInstitutionController', ['$scope', '$modalInstance', 'country', 
-                                                       	function($scope, $modalInstance, country) {
+angular.module('MLDS').controller('AddInstitutionController', ['$scope', '$modalInstance', 'country', '$timeout', '$log',
+                                                       	function($scope, $modalInstance, country, $timeout, $log) {
 	$scope.country = country;
+	$scope.alerts = [];
+	
+	//TODO: AC rename(if needed) and fill in guts to submit new institution
+	$scope.add = function(){
+		$scope.submitting = true;
+		
+		$timeout(function() {
+			$modalInstance.dismiss('cancel');
+		}, 1000);
+	};
+	
+	//TODO: example of adding alerts/errors to modal to be moved into 'add' function
+	$scope.addFail = function(){
+		$scope.submitting = true;
+		
+		$timeout(function() {
+			$scope.alerts.push({type: 'danger', msg: 'Network failure, please try again later.'});
+			$scope.submitting = false;
+		}, 2000);
+	};
+	
+	$scope.closeAlert = function(index) {
+	    $scope.alerts.splice(index, 1);
+	  };
 	
 	$scope.cancel = function() {
 		$modalInstance.dismiss('cancel');
