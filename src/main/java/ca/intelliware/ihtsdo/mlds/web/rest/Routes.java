294688b718459d4892bb0066750676e6f1d7878f
Michael Buckley <michael.buckley@intelliware.com>
2014-06-20 17:57:25 -0400

    Sketch of usage web api

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
@@ -3,7 +3,9 @@ package ca.intelliware.ihtsdo.mlds.web.rest;
 public class Routes {
 	static final String COUNTRIES = "/app/rest/countries";
 	
-	static final String USAGE_REPORTS = "/app/rest/licensees/{licenseeId}/usageReports"; // get list of all submissions including magic "current", post newSnapshotName=name to do submission
-	static final String USAGE_REPORT = "/app/rest/usageReports/{usageReportId}"; // get a single list with bodies of all entries, post to add an entry
-	static final String USAGE_REPORT_ENTRY = "/app/rest/usageReportEntries/{usageReportEntryPublicId}"; // control endpoint for single entry: put to edit, or delete
+	static final String USAGE_REPORTS = "/app/rest/licensees/{licenseeId}/commercialUsages"; // get list of all submissions , post period to do start new submission (auto-copy from most recent)
+	static final String USAGE_REPORT = "/app/rest/commercialUsages/{commercialUsageId}"; // PUT to change submission state
+	// post { submissionState: 'SUBMITTED'}
+	// post { entry : { /* entry json */} }
+	static final String USAGE_REPORT_ENTRY = "/app/rest/commercialUsageEntries/{commercialUsageEntryId}"; // control endpoint for single entry: put to edit, or delete
 }
