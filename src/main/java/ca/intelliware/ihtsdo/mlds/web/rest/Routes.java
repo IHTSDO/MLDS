cbf6eea81aec7caf730a39bcd706c541dc230c68
Michael Buckley <michael.buckley@intelliware.com>
2014-06-23 16:18:47 -0400

    Start commercial usage service

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
@@ -3,9 +3,21 @@ package ca.intelliware.ihtsdo.mlds.web.rest;
 public class Routes {
 	static final String COUNTRIES = "/app/rest/countries";
 	
-	static final String USAGE_REPORTS = "/app/rest/licensees/{licenseeId}/commercialUsages"; // get list of all submissions , post period to do start new submission (auto-copy from most recent)
-	static final String USAGE_REPORT = "/app/rest/commercialUsages/{commercialUsageId}"; // PUT to change submission state
-	// post { submissionState: 'SUBMITTED'}
-	// post { entry : { /* entry json */} }
-	static final String USAGE_REPORT_ENTRY = "/app/rest/commercialUsageEntries/{commercialUsageEntryId}"; // control endpoint for single entry: put to edit, or delete
+	/**
+	 *  - get list of all submissions
+	 *  - post period { startDate, endDate } to do start new submission (auto-copy from most recent)
+	 */
+	static final String USAGE_REPORTS = "/app/rest/licensees/{licenseeId}/commercialUsages"; 
+	
+	
+	/** 
+	 * post { submissionState: 'SUBMITTED'}
+	 * post { entry : { entry json} }
+	 */ 
+	static final String USAGE_REPORT = "/app/rest/commercialUsages/{commercialUsageId}";
+	
+	/**
+	 * control endpoint for single entry: put to edit, or delete
+	 */
+	static final String USAGE_REPORT_ENTRY = "/app/rest/commercialUsages/{commercialUsageId}/entries/{commercialUsageEntryId}"; 
 }
