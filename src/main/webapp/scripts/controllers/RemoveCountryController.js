4abcb548d91cac1fa3aee505f2f1e1a6df9b0cd6
David Jones <david@dgjones.info>
2014-07-02 13:32:43 -0400

    MLDS-26 Provide more context during conformation of commercial usage country removal

--- src/main/webapp/scripts/controllers/RemoveCountryController.js
+++ src/main/webapp/scripts/controllers/RemoveCountryController.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('MLDS').controller('RemoveCountryController', ['$scope', '$modalInstance',  '$log', '$location', 'country', 
-                                                       	function($scope, $modalInstance, $log, $location, country) {
+angular.module('MLDS').controller('RemoveCountryController', ['$scope', '$modalInstance',  '$log', '$location', 'country', 'hospitalsCount', 'practicesCount', 
+                                                       	function($scope, $modalInstance, $log, $location, country, hospitalsCount, practicesCount) {
 	$scope.alerts = [];
 	
 	$scope.country = country;
+	$scope.hospitalsCount = hospitalsCount;
+	$scope.practicesCount = practicesCount;
 	
 	$scope.submit = function(){
 		$scope.submitting = true;
