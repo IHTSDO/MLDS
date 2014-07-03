1e810b5fc943b8f8b1c0a5ac0b05c4fe375d3260
Michael Buckley <michael.buckley@intelliware.com>
2014-07-03 14:16:45 -0400

    MLDS-20 show errors for bad reset tokens

--- src/main/webapp/scripts/controllers/ResetPasswordController.js
+++ src/main/webapp/scripts/controllers/ResetPasswordController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('MLDS').controller('ResetPasswordController', 
-		['$scope', '$log', '$http', 
-        function($scope, $log, $http) {
+		['$scope', '$log', '$resource', '$routeParams',
+        function($scope, $log, $resource, $routeParams) {
 			$scope.resetPassword = {};
 			
 	        $scope.success = null;
@@ -19,6 +19,22 @@ angular.module('MLDS').controller('ResetPasswordController',
 	            } else {
 	                $scope.doNotMatch = null;
 	                $log.log('success', $scope.resetPassword.password);
+	                $resource('/app/rest/resetPassword/:token', {token:$routeParams.token})
+	                	.save({ password : $scope.resetPassword.password })
+	        			.$promise.then(function(data){
+	                        $scope.error = null;
+	                        $scope.success = 'OK';
+	                        // redirect to login
+	        			},function(response){
+	                        $scope.success = null;
+	                        $scope.error={};
+	                        if (response.status == 404) {
+	                        	$scope.error.expired = true;
+	                        } else {
+	                        	$scope.error.server = true;
+	                        }
+	        			});
+
 	                /*
 	                Password.save($scope.password,
 	                    function (value, responseHeaders) {
