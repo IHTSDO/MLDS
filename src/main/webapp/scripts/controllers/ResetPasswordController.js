63c604c030e69b4d7b20c39f37da97511dde3b73
Michael Buckley <michael.buckley@intelliware.com>
2014-07-03 13:21:54 -0400

    MLDS-20 validate passwords on reset form

--- src/main/webapp/scripts/controllers/ResetPasswordController.js
+++ src/main/webapp/scripts/controllers/ResetPasswordController.js
@@ -3,5 +3,32 @@
 angular.module('MLDS').controller('ResetPasswordController', 
 		['$scope', '$log', '$http', 
         function($scope, $log, $http) {
+			$scope.resetPassword = {};
 			
+	        $scope.success = null;
+	        $scope.error = null;
+	        $scope.doNotMatch = null;
+	        $scope.submit = function () {
+        		if ($scope.form.$invalid) {
+        			// FIXME MB can we put this into bs-validation?
+        			$scope.form.attempted = true;
+        			return;
+        		}
+	            if ($scope.resetPassword.password != $scope.resetPassword.confirmPassword) {
+	                $scope.doNotMatch = "ERROR";
+	            } else {
+	                $scope.doNotMatch = null;
+	                $log.log('success', $scope.resetPassword.password);
+	                /*
+	                Password.save($scope.password,
+	                    function (value, responseHeaders) {
+	                        $scope.error = null;
+	                        $scope.success = 'OK';
+	                    },
+	                    function (httpResponse) {
+	                        $scope.success = null;
+	                        $scope.error = "ERROR";
+	                    });*/
+	            }
+	        };
 		}]);
