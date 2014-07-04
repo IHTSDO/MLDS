d81490fb2e7bf6f8434b7c72fe059d082b52c145
Michael Buckley <michael.buckley@intelliware.com>
2014-07-04 15:21:45 -0400

    Start ssl support

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/WebConfigurer.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/WebConfigurer.java
@@ -13,13 +13,22 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 import ca.intelliware.ihtsdo.mlds.web.filter.CachingHttpHeadersFilter;
@@ -58,6 +67,42 @@ public class WebConfigurer implements ServletContextInitializer {
       return new JodaModule();
     }
 
+    
+ // you can run this with SSL/TLS. For example, build the application (`mvn clean install`) in the `oauth` directory, then run:
+    //   java -Dspring.profiles.active=production -Dserver.keystore.file=file:///`pwd`/src/main/resources/keystore.p12 -jar target/oauth-1.0.0.BUILD-SNAPSHOT.jar
+    @Bean
+    @Profile("production")
+    EmbeddedServletContainerCustomizer containerCustomizer(
+            @Value("${server.keystore.file}") Resource keystoreFile,
+            @Value("${server.keystore.pass}") final String keystorePass) throws Exception {
+
+        final String absoluteKeystoreFile = keystoreFile.getFile().getAbsolutePath();
+
+        return new EmbeddedServletContainerCustomizer() {
+			
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+            TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+            tomcat.addConnectorCustomizers(
+                new TomcatConnectorCustomizer() {
+					@Override
+					public void customize(Connector connector) {
+	                    connector.setPort(8443);
+	                    connector.setSecure(true);
+	                    connector.setScheme("https");
+	
+	                    Http11NioProtocol proto = (Http11NioProtocol) connector.getProtocolHandler();
+	                    proto.setSSLEnabled(true);
+	                    proto.setKeystoreFile(absoluteKeystoreFile);
+	                    proto.setKeystorePass(keystorePass);
+	                    //proto.setKeystoreType("jks");
+	                    proto.setKeyAlias("tomcat");
+					}
+                });
+			}
+        };
+    }
+    
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
