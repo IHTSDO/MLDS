c599af41df265ae0752aa656a6b6d673134b3e6b
anson-chung <anson.chung@intelliware.com>
2014-07-07 12:25:38 -0400

    MLDS-173 - Format simple summary of usage countries

--- src/main/webapp/scripts/controllers/AffiliateRegistrationReviewController.js
+++ src/main/webapp/scripts/controllers/AffiliateRegistrationReviewController.js
@@ -4,7 +4,12 @@ mldsApp.controller('AffiliateRegistrationReviewController',
         [ '$scope', '$log', 'UserRegistrationService', '$location', 'UserSession', '$modalInstance', 'CommercialUsageService',
           function ($scope, $log, UserRegistrationService, $location, UserSession, $modalInstance, CommercialUsageService) {
         	$scope.CommercialUsageService = CommercialUsageService;
-
+        	
+        	// FIXME MB this should be on the CommercialUsageService??
+        	$scope.commercialUsageInstitutionsByCountry = 
+        		_.groupBy(CommercialUsageService.currentCommercialUsageReport.entries, 
+        				function(entry){ return entry.country.isoCode2});
+        	
     		$scope.ok = function() {
     			$log.log('AffiliateRegistrationController submit()', $scope.affiliateform);
     			
