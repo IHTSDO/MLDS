af843bd58812f2737519cf446e362228fa2608f1
David Jones <david@dgjones.info>
2014-07-07 16:31:34 -0400

    MLDS-27 More links from usage log back to dashboard

--- src/main/webapp/scripts/controllers/EmbeddableUsageLogController.js
+++ src/main/webapp/scripts/controllers/EmbeddableUsageLogController.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('MLDS').controller('EmbeddableUsageLogController', ['$scope', '$log', '$modal', '$parse', 'CountryService', 'CommercialUsageService', 'Events', 'Session', '$routeParams', 
-                                                 		function($scope, $log, $modal, $parse, CountryService, CommercialUsageService, Events, Session, $routeParams){
+angular.module('MLDS').controller('EmbeddableUsageLogController', ['$scope', '$log', '$modal', '$parse', 'CountryService', 'CommercialUsageService', 'Events', 'Session', '$routeParams', '$location', 
+                                                 		function($scope, $log, $modal, $parse, CountryService, CommercialUsageService, Events, Session, $routeParams, $location){
 	$scope.collapsePanel = {};
 	
 	$scope.usageLogForm = {};
@@ -372,4 +372,7 @@ angular.module('MLDS').controller('EmbeddableUsageLogController', ['$scope', '$l
 	};
 	
 
+	$scope.goToDashboard = function() {
+		$location.path('/dashboard');	
+	};
 }]);
