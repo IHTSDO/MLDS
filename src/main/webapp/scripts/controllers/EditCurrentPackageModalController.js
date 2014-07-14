519898dbd29d6516333aeaa9684ed6fddf5a1cd4
David Jones <david@dgjones.info>
2014-07-14 11:33:49 -0400

    MLDS-265 Use dialog for editing package fields

--- src/main/webapp/scripts/controllers/EditCurrentPackageModalController.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('MLDS').controller('EditCurrentPackageModalController', ['$scope', '$log', '$modalInstance', 'releasePackage', 
-    function ($scope, $log, $modalInstance, releasePackage) {
-		$scope.releasePackage = releasePackage; 
-    }]);
