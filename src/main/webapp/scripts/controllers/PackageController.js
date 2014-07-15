c10853dd3420ec8db07b90323a66ce6f19ac06b7
anson-chung <anson.chung@intelliware.com>
2014-07-15 15:27:38 -0400

    MLDS-256 Watching package entity and updating  version lists on change

--- src/main/webapp/scripts/controllers/PackageController.js
+++ src/main/webapp/scripts/controllers/PackageController.js
@@ -4,6 +4,36 @@ angular.module('MLDS').controller('PackageController',
 		['$scope', '$log', '$routeParams', '$location', '$modal', 'PackagesService',
 		 function($scope, $log, $routeParams, $location, $modal, PackagesService) {
 
+	$scope.versions = {
+			online: [],
+			offline: []
+		};
+	$scope.packageEntity = {};
+	
+	$scope.$watch('packageEntity', function() {
+		var publishedOfflineVersions = [];
+		var nonPublishedOfflineVersions = [];
+		
+		for(var i = 0; i < $scope.packageEntity.releaseVersions.length; i++) {
+			if ($scope.packageEntity.releaseVersions[i].online) {
+				$scope.versions.online.push($scope.packageEntity.releaseVersions[i]);
+			} else {
+				if ($scope.packageEntity.releaseVersions[i].publishedAt) {
+					publishedOfflineVersions.push($scope.packageEntity.releaseVersions[i]);
+				} else {
+					nonPublishedOfflineVersions.push($scope.packageEntity.releaseVersions[i]);
+				}
+			}
+		};
+		
+		$scope.versions.online.sort(function(a,b){return new Date(b.publishedAt) - new Date(a.publishedAt);});
+		publishedOfflineVersions.sort(function(a,b){return new Date(b.publishedAt) - new Date(a.publishedAt);});
+		nonPublishedOfflineVersions.sort(function(a,b){return new Date(b.createdAt) - new Date(a.createdAt);});
+		$scope.versions.offline = publishedOfflineVersions.concat(nonPublishedOfflineVersions);
+		
+	});
+	
+			
 	var releasePackageId = $routeParams.packageId && parseInt($routeParams.packageId, 10);
 	var loadReleasePackage = function loadReleasePackage() {
 		if (releasePackageId) {
