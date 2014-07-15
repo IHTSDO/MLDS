dad633e34bc6e18ab7b371f34f0986ca1f47c171
Michael Buckley <michael.buckley@intelliware.com>
2014-07-15 12:29:42 -0400

    MLDS-256 first cut of add version ui

--- src/main/webapp/scripts/controllers/PackageController.js
+++ src/main/webapp/scripts/controllers/PackageController.js
@@ -18,6 +18,25 @@ angular.module('MLDS').controller('PackageController',
 		$location.path('/packageManagement');
 	};
 	
+	$scope.addReleaseVersion = function addReleaseVersion() {
+        var modalInstance = $modal.open({
+            templateUrl: 'views/admin/addReleaseVersionModal.html', // FM
+            controller: 'AddReleaseVersionModalController', // FM
+            scope: $scope,
+            size: 'lg',
+            backdrop: 'static',
+            resolve: {
+              releasePackage: function() {
+              	// FIXME not sure about copy - needed to support modal cancel or network failure
+              	return angular.copy($scope.packageEntity);
+              }
+            }
+          });
+      modalInstance.result.then(function(updatedReleasePackage) {
+      	$scope.packageEntity = updatedReleasePackage;
+      });
+	}
+	
     $scope.editReleasePackage = function() {
         var modalInstance = $modal.open({
               templateUrl: 'views/admin/editPackageModal.html',
