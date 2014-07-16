53e23ae021b58b979a03134ba1e7e484edb3fce8
anson-chung <anson.chung@intelliware.com>
2014-07-16 15:31:36 -0400

    MLDS-271 delete all release files associated to version before deleting version

--- src/main/webapp/scripts/controllers/DeleteVersionModalController.js
+++ src/main/webapp/scripts/controllers/DeleteVersionModalController.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('MLDS').controller('DeleteVersionModalController', ['$scope', '$log', '$modalInstance', 'releasePackage', 'releaseVersion', 'ReleaseVersionsService', 
-    function ($scope, $log, $modalInstance, releasePackage, releaseVersion, ReleaseVersionsService) {
+angular.module('MLDS').controller('DeleteVersionModalController', ['$scope', '$log', '$modalInstance', 'releasePackage', 'releaseVersion', 'ReleaseVersionsService', 'ReleaseFilesService', 
+    function ($scope, $log, $modalInstance, releasePackage, releaseVersion, ReleaseVersionsService, ReleaseFilesService) {
 		$scope.releasePackage = releasePackage;
 		$scope.releaseVersion = releaseVersion;
 		$scope.alerts = [];
@@ -9,6 +9,13 @@ angular.module('MLDS').controller('DeleteVersionModalController', ['$scope', '$l
 		$scope.ok = function() {
 			$scope.submitting = true;
 			$scope.alerts.splice(0, $scope.alerts.length);
+			
+			for(var i = 0; i < releaseVersion.releaseFiles.length; i++) {
+				ReleaseFilesService['delete'](
+						{	releasePackageId: $scope.packageEntity.releasePackageId,
+							releaseVersionId: releaseVersion.releaseVersionId,
+							releaseFileId: releaseVersion.releaseFiles[i].releaseFileId	});
+			};
 			
 			ReleaseVersionsService['delete']({releasePackageId : releasePackage.releasePackageId}, $scope.releaseVersion)
 				.$promise.then(function(result) {
