1d3f18f42086167690756ef16710ad1ff86d85de
Michael Buckley <michael.buckley@intelliware.com>
2014-07-16 18:00:48 -0400

    MLDS-50 dump package data onto browse pages

--- src/main/webapp/scripts/controllers/ViewPackageController.js
+++ src/main/webapp/scripts/controllers/ViewPackageController.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('MLDS')
-    .controller('ViewPackageController', ['$scope', function($scope){
+    .controller('ViewPackageController', ['$scope', '$routeParams', 'PackagesService', function($scope, $routeParams, PackagesService){
     	
+	var releasePackageId = $routeParams.releasePackageId && parseInt($routeParams.releasePackageId, 10);
+	
+	var loadReleasePackage = function loadReleasePackage() {
+		if (releasePackageId) {
+			PackagesService.get({releasePackageId: releasePackageId})
+			.$promise.then(function(result) {
+				$scope.releasePackage = result;
+				})
+					["catch"](function(message) {
+						//FIXME how to handle errors + not present
+						$log.log('ReleasePackage not found');
+						$location.path('/packageManagement');
+					});
+		} else {
+			$location.path('/viewPackages');
+		};
+	}
+
+	loadReleasePackage();
+
     }]);
