0cfbc07b81fb3d5b34716bb29d5e2e2045021273
anson-chung <anson.chung@intelliware.com>
2014-07-21 13:41:33 -0400

    Adding "View Packages" link to application approval email

--- src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/EmailVariables.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/EmailVariables.java
@@ -4,5 +4,6 @@ public class EmailVariables {
 	static final String USER = "user";
 	static final String PASSWORD_RESET_URL = "passwordResetUrl";
 	static final String LOGIN_URL = "loginUrl";
+	static final String VIEW_PACKAGES_URL = "viewPackagesUrl";
 	
 }
