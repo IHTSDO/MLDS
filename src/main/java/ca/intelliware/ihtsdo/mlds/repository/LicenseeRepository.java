ded7665c4b6268578f5b19bd8bf567a31003f286
Michael Buckley <michael.buckley@intelliware.com>
2014-07-20 22:28:48 -0400

    MLDS-274 do dumb text search to flesh out ui

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/LicenseeRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/LicenseeRepository.java
@@ -3,9 +3,16 @@ package ca.intelliware.ihtsdo.mlds.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ca.intelliware.ihtsdo.mlds.domain.Licensee;
 
 public interface LicenseeRepository extends JpaRepository<Licensee, Long> {
 	List<Licensee> findByCreator(String userName);
+
+	@Query(value="select l.* from licensee l join application a on a.application_id = l.application_id "
+			+ "where lower(a.full_name) like '%' || ?1 || '%' "
+			+ "or lower(a.organization_name) like  '%' || ?1 || '%' "
+			+ "or lower(a.street) like  '%' || ?1 || '%'", nativeQuery=true)
+	List<Licensee> findByTextQuery(String q);
 }
--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/LicenseeResource.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/LicenseeResource.java
@@ -1,6 +1,7 @@
 package ca.intelliware.ihtsdo.mlds.web.rest;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.annotation.security.RolesAllowed;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,7 @@ import ca.intelliware.ihtsdo.mlds.repository.LicenseeRepository;
 import ca.intelliware.ihtsdo.mlds.security.AuthoritiesConstants;
 import ca.intelliware.ihtsdo.mlds.web.SessionService;
 
+import com.google.common.base.Strings;
 import com.wordnik.swagger.annotations.Api;
 
 @RestController
@@ -37,8 +40,14 @@ public class LicenseeResource {
     @RequestMapping(value = Routes.LICENSEES,
     		method = RequestMethod.GET,
             produces = "application/json")
-    public @ResponseBody ResponseEntity<Collection<Licensee>> getLicensees() {
-    	return new ResponseEntity<Collection<Licensee>>(licenseeRepository.findAll(), HttpStatus.OK);
+    public @ResponseBody ResponseEntity<Collection<Licensee>> getLicensees(@RequestParam String q) {
+		List<Licensee> licensees;
+		if (!Strings.isNullOrEmpty(q)) {
+			licensees = licenseeRepository.findByTextQuery("%" + q.toLowerCase() + "%");
+		} else {
+			licensees = licenseeRepository.findAll();
+		}
+		return new ResponseEntity<Collection<Licensee>>(licensees, HttpStatus.OK);
     }
 
 	
