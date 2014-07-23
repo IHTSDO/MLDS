b036fb326b984328ea093df3da7260a04145a2cd
anson-chung <anson.chung@intelliware.com>
2014-07-23 09:18:37 -0400

    MLDS-259 renaming settings page to contact info

---
+++ src/main/webapp/scripts/controllers/ContactInfoController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('MLDS').controller('ContactInfoController', ['$scope', 'Account',
+    function ($scope, Account) {
+        $scope.success = null;
+        $scope.error = null;
+        $scope.settingsAccount = Account.get();
+
+        $scope.save = function () {
+            Account.save($scope.settingsAccount,
+                function (value, responseHeaders) {
+                    $scope.error = null;
+                    $scope.success = 'OK';
+                    $scope.settingsAccount = Account.get();
+                },
+                function (httpResponse) {
+                    $scope.success = null;
+                    $scope.error = "ERROR";
+                });
+        };
+    }]);
