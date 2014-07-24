6becd01f909270b1ac2648d2e001ce2cf74984bb
anson-chung <anson.chung@intelliware.com>
2014-07-24 14:19:23 -0400

    MLDS-259 making mailing addresses embedded objects

--- src/main/java/ca/intelliware/ihtsdo/mlds/domain/AffiliateDetails.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/domain/AffiliateDetails.java
@@ -1,11 +1,15 @@
 package ca.intelliware.ihtsdo.mlds.domain;
 
+import javax.persistence.AssociationOverride;
+import javax.persistence.AssociationOverrides;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -32,20 +36,26 @@ public class AffiliateDetails {
 	@Column(name="third_email")
 	String thirdEmail;
 	
-	@OneToOne
-	@JoinColumn(name="address_id")
+	@AttributeOverrides({
+		@AttributeOverride(name="street", column=@Column(name="street")),
+		@AttributeOverride(name="city", column=@Column(name="city")),
+		@AttributeOverride(name="post", column=@Column(name="post"))
+	})
+	@AssociationOverrides(@AssociationOverride(name="country", joinColumns=@JoinColumn(name="country_iso_code_2")))
+	@Embedded
 	MailingAddress address;
 	
 	@Column(name="organization_name")
 	String organizationName;
 	
-	@OneToOne()
-	@JoinColumn(name="organization_address_id")
-	MailingAddress organizationAddress;
-	
-	@OneToOne()
-	@JoinColumn(name="organization_billing_address_id")
-	MailingAddress organizationBillingAddress;
+	@AttributeOverrides({
+		@AttributeOverride(name="street", column=@Column(name="billing_street")),
+		@AttributeOverride(name="city", column=@Column(name="billing_city")),
+		@AttributeOverride(name="post", column=@Column(name="billing_post"))
+	})
+	@AssociationOverrides(@AssociationOverride(name="country", joinColumns=@JoinColumn(name="billing_country_iso_code_2")))
+	@Embedded
+	MailingAddress billingAddress;
 
 	public Long getAffiliateDetailsId() {
 		return affiliateDetailsId;
@@ -111,21 +121,13 @@ public class AffiliateDetails {
 		this.organizationName = organizationName;
 	}
 
-	public MailingAddress getOrganizationAddress() {
-		return organizationAddress;
+	public MailingAddress getBillingAddress() {
+		return billingAddress;
 	}
 
-	public void setOrganizationAddress(MailingAddress organizationAddress) {
-		this.organizationAddress = organizationAddress;
-	}
-
-	public MailingAddress getOrganizationBillingAddress() {
-		return organizationBillingAddress;
-	}
-
-	public void setOrganizationBillingAddress(
+	public void setBillingAddress(
 			MailingAddress organizationBillingAddress) {
-		this.organizationBillingAddress = organizationBillingAddress;
+		this.billingAddress = organizationBillingAddress;
 	}
 	
 	
