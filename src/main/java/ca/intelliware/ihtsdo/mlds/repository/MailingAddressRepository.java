9e338dd9cb8962d481ce33b13b05440d926bdf44
anson-chung <anson.chung@intelliware.com>
2014-07-24 14:19:23 -0400

    MLDS-259 making mailing addresses embedded objects

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/MailingAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package ca.intelliware.ihtsdo.mlds.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ca.intelliware.ihtsdo.mlds.domain.AffiliateDetails;
-
-public interface MailingAddressRepository extends JpaRepository<AffiliateDetails, Long> {
-}
