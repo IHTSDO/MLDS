3b2b27351df0c26279822adeda6169b3828ee130
anson-chung <anson.chung@intelliware.com>
2014-07-25 15:41:45 -0400

    MLDS-259 refactoring affiliate registration controller to use same names as backend

--- src/main/webapp/scripts/controllers/AffiliateRegistrationController.js
+++ src/main/webapp/scripts/controllers/AffiliateRegistrationController.js
@@ -11,40 +11,10 @@ mldsApp.controller('AffiliateRegistrationController',
         		
         		queryPromise.success(function(data) {
     				$log.log("loadApplication", data);
-    				var affiliateDetails = data.affiliateDetails ? data.affiliateDetails : {};
-    				var address = affiliateDetails.address ? affiliateDetails.address : {};
-    				var billingAddress = affiliateDetails.billingAddress ? affiliateDetails.billingAddress : {};
-    				
+    				$scope.affiliateform = data;
     				$scope.approvalState = data.approvalState;
     				$scope.applicationId = data.applicationId;
-    				
-    				$scope.affiliateform.type = data.type ? data.type : '';
-    				$scope.affiliateform.usageSubType = data.subType;
-    				$scope.affiliateform.otherText = data.otherText ? data.otherText : '';
-    				
-    				$scope.affiliateform.contact.firstName = 
-    				
-    				$scope.affiliateform.contact.firstName = affiliateDetails.firstName ? affiliateDetails.firstName : Session.firstName;
-    				$scope.affiliateform.contact.lastName = affiliateDetails.lastName ? affiliateDetails.lastName : Session.lastName;
-    				$scope.affiliateform.contact.email = affiliateDetails.email ? affiliateDetails.email : Session.email;
-    				$scope.affiliateform.contact.alternateEmail = affiliateDetails.alternateEmail ? affiliateDetails.alternateEmail : '';
-    				$scope.affiliateform.contact.thirdEmail = affiliateDetails.thirdEmail ? affiliateDetails.thirdEmail : '';
-    				$scope.affiliateform.contact.landlineNumber = affiliateDetails.landlineNumber ? affiliateDetails.landlineNumber : '';
-    				$scope.affiliateform.contact.landlineExtension = affiliateDetails.landlineExtension ? affiliateDetails.landlineExtension : '';
-    				$scope.affiliateform.contact.mobileNumber = affiliateDetails.mobileNumber ? affiliateDetails.mobileNumber : '';
-    				$scope.affiliateform.organization.name = affiliateDetails.organizationName ? affiliateDetails.organizationName : '';
-    				$scope.affiliateform.organization.type = affiliateDetails.organizationType ? affiliateDetails.organizationType : '';
-    				$scope.affiliateform.organization.typeOther = affiliateDetails.organizationTypeOther ? affiliateDetails.organizationTypeOther : '';
-    				
-    				$scope.affiliateform.address.street = address.street ? address.street : '';
-    				$scope.affiliateform.address.city = address.city ? address.city : '';
-    				$scope.affiliateform.address.post = address.post ? address.post : '';
-    				$scope.affiliateform.address.country = address.country ? address.country : '';
-    				$scope.affiliateform.billing.street = billingAddress.street ? billingAddress.street : '';
-    				$scope.affiliateform.billing.city = billingAddress.city ? billingAddress.city : '';
-    				$scope.affiliateform.billing.postCode = billingAddress.post ? billingAddress.post : '';
-    				$scope.affiliateform.billing.country = billingAddress.country ? billingAddress.country : '';
-    				$scope.isSameAddress = checkAddresses(address, billingAddress);
+    				$scope.isSameAddress = checkAddresses($scope.affiliateform.affiliateDetails.address, $scope.affiliateform.affiliateDetails.billingAddress);
     				
     				if (!UserRegistrationService.isApplicationWaitingForApplicant(data)) {
     					$log.log('Application does not require input from applicant');
@@ -63,20 +33,29 @@ mldsApp.controller('AffiliateRegistrationController',
         	$scope.applicationId = null;
         	
             // bind the display name to our country object.
-            $scope.$watch('affiliateform.address.country', function(newValue){
+            $scope.$watch('affiliateform.affiliateDetails.address.country', function(newValue){
             	var country = _.findWhere(CountryService.countries, {'commonName':newValue});
             	$scope.selectedCountry = country;
             	var excludedCountry = country && country.excludeRegistration;
             	$scope.affiliateApplicationForm.country.$setValidity('excluded',!excludedCountry);
+            	$scope.affiliateApplicationForm.billingCountry.$setValidity('excluded',!excludedCountry);
             	$scope.affiliateApplicationForm.countryIndividual.$setValidity('excluded',!excludedCountry);
             });
-        	
+            $scope.$watch('affiliateform.affiliateDetails.billingAddress.country', function(newValue){
+            	var country = _.findWhere(CountryService.countries, {'commonName':newValue});
+            	$scope.selectedCountry = country;
+            	var excludedCountry = country && country.excludeRegistration;
+            	$scope.affiliateApplicationForm.country.$setValidity('excluded',!excludedCountry);
+            	$scope.affiliateApplicationForm.billingCountry.$setValidity('excluded',!excludedCountry);
+            	$scope.affiliateApplicationForm.countryIndividual.$setValidity('excluded',!excludedCountry);
+            });
+            
         	window.regScope = $scope;
         	$scope.affiliateform = {};
         	
-        	$scope.affiliateform.contact = {};
-        	$scope.affiliateform.address = {};
-        	$scope.affiliateform.billing = {};
+        	$scope.affiliateform.affiliateDetails = {};
+        	$scope.affiliateform.affiliateDetails.address = {};
+        	$scope.affiliateform.affiliateDetails.billingAddress = {};
         	$scope.affiliateform.organization = {};
         	
         	$scope.saveApplication = function() {
@@ -116,11 +95,11 @@ mldsApp.controller('AffiliateRegistrationController',
         	
         	$scope.copyAddress = function() {
         		if($scope.isSameAddress) {
-        			$scope.affiliateform.billing = {};
-        			$scope.affiliateform.billing.street = $scope.affiliateform.address.street;
-        			$scope.affiliateform.billing.city = $scope.affiliateform.address.city;
-        			$scope.affiliateform.billing.post = $scope.affiliateform.address.post;
-        			$scope.affiliateform.billing.country = $scope.affiliateform.address.country;
+        			$scope.affiliateform.affiliateDetails.billingAddress = {};
+        			$scope.affiliateform.affiliateDetails.billingAddress.street = $scope.affiliateform.affiliateDetails.address.street;
+        			$scope.affiliateform.affiliateDetails.billingAddress.city = $scope.affiliateform.affiliateDetails.address.city;
+        			$scope.affiliateform.affiliateDetails.billingAddress.post = $scope.affiliateform.affiliateDetails.address.post;
+        			$scope.affiliateform.affiliateDetails.billingAddress.country = $scope.affiliateform.affiliateDetails.address.country;
         		};
         	};
         	
