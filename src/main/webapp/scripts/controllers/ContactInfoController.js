de148af8bf5d6a842f00afdac63a5e88f7e1834d
David Jones <david@dgjones.info>
2014-07-25 12:19:12 -0400

    MLDS-259 MLDS-354 Start updating contact info with registration like form

--- src/main/webapp/scripts/controllers/ContactInfoController.js
+++ src/main/webapp/scripts/controllers/ContactInfoController.js
@@ -1,11 +1,68 @@
 'use strict';
 
-angular.module('MLDS').controller('ContactInfoController', ['$scope', 'Account',
-    function ($scope, Account) {
+angular.module('MLDS').controller('ContactInfoController', ['$scope', '$log', 'Account', 'AffiliateService', 'CountryService',
+    function ($scope, $log, Account, AffiliateService, CountryService) {
         $scope.success = null;
         $scope.error = null;
         $scope.settingsAccount = Account.get();
+        $scope.availableCountries = CountryService.countries;
+        
+        //FIXME review fields
+        $scope.affiliate = null;
+        $scope.affiliateDetails = null;
+        $scope.type = null;
 
+        function insertFakeDetails(affiliate) {
+        	affiliate.affiliateDetails = {
+        			firstName: 'John',
+        			lastName: 'Smith',
+        			email: 'email@com',
+        			alternateEmail: 'alternative@com',
+        			thirdEmail: 'third@com',
+        			address: {
+	        			street: 'street',
+	        			city: 'city',
+	        			post: 'post',
+	        			country: 'Canada'
+        			},
+        			organizationName: 'Organization Name',
+        			billingAddress: {
+	        			street: 'b street',
+	        			city: 'b city',
+	        			post: 'b post',
+	        			country: 'Botswana'
+        			},
+        			landlineNumber: '+1 4156 762 0032',
+        			landlineExtension: '123',
+        			mobileNumber: '+1 416 999 99999'
+        			
+        	};
+        }
+        
+        function loadAffiliate() {
+        	//FIXME use single affiliate service call...
+        	AffiliateService.myAffiliates()
+        		.then(function(result) {
+        			var affiliates = result.data;
+        			if (affiliates && affiliates.length > 0) {
+        				$scope.affiliate = affiliates[0];
+        				$scope.type = 'INDIVIDUAL'/*$scope.affiliate.type*/;
+        				insertFakeDetails($scope.affiliate);
+        				$scope.affiliateDetails = $scope.affiliate.affiliateDetails;
+        				$log.log($scope.affiliate);
+        			} else {
+        				$log.log('No affiliates found...');
+        			}
+        		})
+    			["catch"](function(message) {
+    				//FIXME handle affiliate loading error
+    				$log.log('Error loading my affiliate');
+    			});
+
+        }
+        
+        loadAffiliate();
+        
         $scope.save = function () {
             Account.save($scope.settingsAccount,
                 function (value, responseHeaders) {
@@ -18,4 +75,6 @@ angular.module('MLDS').controller('ContactInfoController', ['$scope', 'Account',
                     $scope.error = "ERROR";
                 });
         };
+        
+        
     }]);
