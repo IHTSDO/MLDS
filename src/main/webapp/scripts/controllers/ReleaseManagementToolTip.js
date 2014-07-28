c20f2f09e69a1fa373ef39230d3cb019a248e93e
Michelle Andrews <michelle.andrews@intelliware.com>
2014-07-28 12:51:49 -0400

    removed unnecceary file

--- src/main/webapp/scripts/controllers/ReleaseManagementToolTip.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('MLDS', ['ui.bootstrap']);
-var ToolTip = function ($scope) {
-  $scope.dynamicPopover = 'Hello, World!';
-  $scope.dynamicPopoverTitle = 'Title';
-};
