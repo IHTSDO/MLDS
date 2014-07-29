6de90f0790fad09968899237494af9b6ad1b6db7
Michael Buckley <michael.buckley@intelliware.com>
2014-07-29 19:33:16 -0400

    MLDS-372 Trim down Member serialized form

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
@@ -1,8 +1,10 @@
 package ca.intelliware.ihtsdo.mlds.config;
 
+import java.io.IOException;
 import java.util.Collection;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
@@ -11,10 +13,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import ca.intelliware.ihtsdo.mlds.domain.Member;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @Configuration
 @ConditionalOnClass(ObjectMapper.class)
@@ -23,6 +35,34 @@ public class JacksonConfigurer {
 	@Autowired
 	private ListableBeanFactory beanFactory;
 
+	@Bean
+	public Module mldsModule(final EntityManager em) {
+		SimpleModule mldsModule = new SimpleModule("MLDS Jackson");
+		
+		mldsModule.addSerializer(Member.class, new JsonSerializer<Member>() {
+			@Override
+			public void serialize(Member value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+				jgen.writeStartObject();
+				jgen.writeObjectField("key", value.getKey());
+				jgen.writeEndObject();
+			}});
+		mldsModule.addDeserializer(Member.class, new JsonDeserializer<Member>() {
+			@Override
+			public Member deserialize(JsonParser jp, DeserializationContext ctxt)
+					throws IOException, JsonProcessingException {
+				JsonNode node = jp.getCodec().readTree(jp);
+				String key = node.get("key").textValue();
+				Member result = (Member) em
+						.createQuery("from Member where key = :key")
+						.setParameter("key", key)
+						.getSingleResult();
+				return result;
+			}
+		});
+		
+		return mldsModule;
+	}
+	
 	@PostConstruct
 	public void init() {
 		Collection<ObjectMapper> mappers = BeanFactoryUtils
