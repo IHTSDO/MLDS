f64a37d653a2512279f69b74b4c588fadcb959ce
David Jones <david@dgjones.info>
2014-07-30 16:59:07 -0400

    MLDS-300 MLDS-415 Add placeholder Affiliates staff/admin page

---
+++ src/main/webapp/scripts/controllers/AffiliatesController.js
@@ -0,0 +1,48 @@
+'use strict';
+
+mldsApp.controller('AffiliatesController', [
+		'$scope',
+		'$log',
+		'$location',
+		'Session',
+		'UserRegistrationService',
+		'DomainBlacklistService',
+		'PackagesService',
+		'AffiliateService',
+		function($scope, $log, $location, Session, UserRegistrationService, DomainBlacklistService,
+				PackagesService, AffiliateService) {
+
+			$scope.affiliates = [];
+			$scope.showAllAffiliates = false;
+			$scope.affiliatesFilter = '';
+
+			$scope.alerts = [];
+
+			function loadAffiliates() {
+				AffiliateService.affiliatesResource.query({q:''}).$promise
+					.then(function(response) {
+						$log.log('response', response, response.length);
+						$scope.affiliates = response;
+						_.sortBy($scope.affiliates, function(record) {
+							return record.creator;
+						});
+					})
+					["catch"](function(message) {
+						$scope.alerts.push({type: 'danger', msg: 'Network failure, please try again later.'});
+					});
+			}
+
+			loadAffiliates();
+			
+			$scope.$watch('showAllAffiliates', toggleAffiliates);
+			
+			function toggleAffiliates() {
+				if ($scope.affiliatesFilter) {
+					$scope.affiliatesFilter = '';
+				} else {
+					var memberKey = Session.member && Session.member.key || 'NONE';
+					$scope.affiliatesFilter = {'homeMember': memberKey};
+				}
+			};
+
+		} ]);
