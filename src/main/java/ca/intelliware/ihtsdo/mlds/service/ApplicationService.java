65bdb28c00d5cfe8180e8fbeb6be8bb3a9a4ad0d
Michael Buckley <michael.buckley@intelliware.com>
2014-07-31 17:17:42 -0400

    MLDS-308 Create application by posting applicationType to /applications route

--- src/main/java/ca/intelliware/ihtsdo/mlds/service/ApplicationService.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/service/ApplicationService.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Service;
 import ca.intelliware.ihtsdo.mlds.domain.Application;
 import ca.intelliware.ihtsdo.mlds.domain.Application.ApplicationType;
 import ca.intelliware.ihtsdo.mlds.repository.ApplicationRepository;
+import ca.intelliware.ihtsdo.mlds.web.SessionService;
+import ca.intelliware.ihtsdo.mlds.web.rest.UserMembershipAccessor;
 
 @Service
 public class ApplicationService {
 
 	@Resource ApplicationRepository applicationRepository;
+	@Resource SessionService sessionService;
+	@Resource UserMembershipAccessor userMembershipAccessor; 
+
 
 	public Application startNewApplication(ApplicationType applicationType) {
 		Application application = Application.create(applicationType);
+		
+		application.setUsername(sessionService.getUsernameOrNull());
+		application.setMember(userMembershipAccessor.getMemberAssociatedWithUser());
 		// FIXME MLDS-308 MB fill in more details
 		
 		applicationRepository.save(application);
