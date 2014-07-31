ed20a16134d292734e90070a4c85a5d9f0c375f6
Michael Buckley <michael.buckley@intelliware.com>
2014-07-31 16:50:06 -0400

    Create RouteLinkBuilder to render urls back to client

---
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/RouteLinkBuilderTest.java
@@ -0,0 +1,34 @@
+package ca.intelliware.ihtsdo.mlds.web.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+
+public class RouteLinkBuilderTest {
+	RouteLinkBuilder routeLinkBuilder = new RouteLinkBuilder();
+
+	@Test
+	public void routeWithNoTokensIsUnmolested() throws URISyntaxException {
+		URI uri = routeLinkBuilder.toURLWithKeyValues("/aRoute/");
+		
+		assertEquals(new URI("/aRoute/"), uri);
+	}
+	
+	@Test
+	public void simpleTokenIsReplaced() throws URISyntaxException {
+		URI uri = routeLinkBuilder.toURLWithKeyValues("/aRoute/{pathVariable}", "pathVariable", 55);
+		
+		assertEquals(new URI("/aRoute/55"), uri);
+	}
+	
+	@Test
+	public void tokenReplacementIsURLEscaped() throws URISyntaxException {
+		URI uri = routeLinkBuilder.toURLWithKeyValues("/aRoute/{pathVariable}", "pathVariable", "escape?");
+		
+		assertEquals(new URI("/aRoute/escape%3F"), uri);
+	}
+
+}
