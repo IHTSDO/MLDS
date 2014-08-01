a190fcbb2c8d5fee4dc8cb24ff27b384f21e60d1
David Jones <david@dgjones.info>
2014-08-01 17:06:29 -0400

    MLDS-308 MLDS-421 Control Release Package login/apply button for download

--- src/main/webapp/scripts/controllers/ViewPackageController.js
+++ src/main/webapp/scripts/controllers/ViewPackageController.js
@@ -2,8 +2,8 @@
 
 angular.module('MLDS')
     .controller('ViewPackageController', 
-    		['$scope', '$routeParams', 'PackagesService', 'PackageUtilsService', '$location', '$log',
-          function($scope, $routeParams, PackagesService, PackageUtilsService, $location, $log){
+    		['$scope', '$routeParams', 'PackagesService', 'PackageUtilsService', '$location', '$log', 'UserAffiliateService',
+          function($scope, $routeParams, PackagesService, PackageUtilsService, $location, $log, UserAffiliateService){
     	
 	var releasePackageId = $routeParams.releasePackageId && parseInt($routeParams.releasePackageId, 10);
 	
@@ -13,18 +13,31 @@ angular.module('MLDS')
 		};
 	$scope.releasePackage = {releaseVersions:[]};
 	
+	$scope.isMembershipApproved = false;
+	$scope.isMembershipIncomplete = false;
+	$scope.isMembershipUnstarted = false;
+	
 	$scope.utils = PackageUtilsService;
 	
 	$scope.$watch('releasePackage', function(newValue, oldValue) {
 		$scope.releaseVersions = $scope.utils.updateVersionsLists(newValue);
 	});
 	
+	var setReleasePackage = function setReleasePackage(releasePackage) {
+		$scope.releasePackage = releasePackage;
+		$log.log(releasePackage);
+		UserAffiliateService.promise.then(function() {
+			$scope.isMembershipApproved = _.some(UserAffiliateService.approvedMemberships, function(member) { return member.key === releasePackage.member.key;});
+			$scope.isMembershipIncomplete = _.some(UserAffiliateService.incompleteMemberships, function(member) {return member.key === releasePackage.member.key;});
+			$scope.isMembershipUnstarted = !$scope.isMembershipApproved && !$scope.isMembershipIncomplete;
+		});
+	};
+
 	var loadReleasePackage = function loadReleasePackage() {
 		if (releasePackageId) {
 			PackagesService.get({releasePackageId: releasePackageId})
 			.$promise.then(function(result) {
-				$scope.releasePackage = result;
-				$log.log(result);
+				setReleasePackage(result);
 				})
 					["catch"](function(message) {
 						//FIXME how to handle errors + not present 
@@ -41,6 +54,7 @@ angular.module('MLDS')
 	$scope.goToViewPackages = function goToViewPackages() {
 		$location.path('/viewPackages');
 	};
+	
 }]);
 
 
