401f7b367b95660b7c590bf1205b7c5030f8c234
David Jones <david@dgjones.info>
2014-08-01 15:58:01 -0400

    MLDS-308 MLDS-421 Order release packages by approval state

--- src/main/webapp/scripts/controllers/ViewPackagesController.js
+++ src/main/webapp/scripts/controllers/ViewPackagesController.js
@@ -2,8 +2,8 @@
 
 angular.module('MLDS')
     .controller('ViewPackagesController', 
-    		['$scope', '$log', 'PackagesService', 'PackageUtilsService', '$location',
-           function ($scope, $log, PackagesService, PackageUtilsService, $location) {
+    		['$scope', '$log', 'PackagesService', 'PackageUtilsService', '$location', 'MemberService', 'UserAffiliateService',
+           function ($scope, $log, PackagesService, PackageUtilsService, $location, MemberService, UserAffiliateService) {
 			
 	$scope.utils = PackageUtilsService;
 	$scope.releasePackagesByMember = [];
@@ -22,11 +22,28 @@ angular.module('MLDS')
 				$scope.releasePackagesByMember = _.chain(releasePackages)
 					.filter(PackageUtilsService.isPackagePublished)
 					.groupBy(function(value) {return value.member.key;})
-					.pairs()
+					.map(function(packages, memberKey) {
+						return {
+							member: MemberService.membersByKey[memberKey], 
+							packages: packages};})
 					.value();
 			})
 			["catch"](function(message) {
 				$scope.alerts.push({type: 'danger', msg: 'Network failure, please try again later.'});
 			});
 	}
+	
+	$scope.orderIhtsdo = function(memberReleases) {
+		return !(MemberService.ihtsdoMemberKey === memberReleases.member.key);
+	};
+	$scope.orderApprovedMemberships = function(memberReleases) {
+		return !(_.some(UserAffiliateService.approvedMemberships, memberReleases.member));
+	};
+	$scope.orderIncompleteMemberships = function(memberReleases) {
+		return !(_.some(UserAffiliateService.incompleteMemberships, memberReleases.member));
+	};
+	$scope.orderMemberName = function(memberReleases) {
+		//FIXME use translated member name rather than key
+		return  memberReleases.member.key;
+	};
 }]);
--- src/main/webapp/scripts/services/UserAffiliateService.js
+++ src/main/webapp/scripts/services/UserAffiliateService.js
@@ -1,23 +1,37 @@
 'use strict';
 
 angular.module('MLDS')
-.factory('UserAffiliateService', ['$http', '$rootScope', '$log', '$q', 'Session', 'AffiliateService',
-                                    function($http, $rootScope, $log, $q, Session, AffiliateService){
+.factory('UserAffiliateService', ['$http', '$rootScope', '$log', '$q', 'Session', 'AffiliateService', 'UserRegistrationService',
+                                    function($http, $rootScope, $log, $q, Session, AffiliateService, UserRegistrationService){
 	var service = {
-		affiliate: null
+		affiliate: null,
+		approvedMemberships: [],
+		incompleteMemberships: []
 	};
 	
 	var loadUserAffiliate = function loadUserAffiliate() {
 		service.affiliate = null;
 		service.promise = AffiliateService.myAffiliate();
 		service.promise.then(function(resp){
-			service.affiliate = resp.data;
+			setAffiliate(resp.data);
 		});
 	};
 	loadUserAffiliate();
 	
 	$rootScope.$on('event:auth-loginConfirmed', loadUserAffiliate);
 	$rootScope.$on('event:auth-loginCancelled', loadUserAffiliate);
+	
+	var setAffiliate = function setAffiliate(affiliate) {
+		service.affiliate = affiliate;
+		service.approvedMemberships = _.chain(service.affiliate.applications)
+			.filter(UserRegistrationService.isApplicationApproved)
+			.pluck('member')
+			.value();
+		service.incompleteMemberships = _.chain(service.affiliate.applications)
+			.filter(UserRegistrationService.isApplicationIncomplete)
+			.pluck('member')
+			.value();
+	};
 	
 	return service;
 }]);
