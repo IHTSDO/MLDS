ee73b7e9884a677f6be3097d7b585257ff3de644
anson-chung <anson.chung@intelliware.com>
2014-08-05 12:02:07 -0400

    Updating query to look at affiliate details for matching fields

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/AffiliateRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/AffiliateRepository.java
@@ -10,9 +10,10 @@ import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
 public interface AffiliateRepository extends JpaRepository<Affiliate, Long> {
 	List<Affiliate> findByCreator(String userName);
 
-	@Query(value="select l.* from affiliate l join application a on a.application_id = l.application_id "
-			+ "where lower(a.full_name) like '%' || ?1 || '%' "
-			+ "or lower(a.organization_name) like  '%' || ?1 || '%' "
-			+ "or lower(a.street) like  '%' || ?1 || '%'", nativeQuery=true)
+	@Query(value="SELECT l.* from affiliate l JOIN application a on a.application_id = l.application_id JOIN affiliate_details b on b.affiliate_details_id = a.affiliate_details_id "
+			+ "where lower(b.first_name) like '%' || ?1 || '%' "
+			+ "or lower(b.last_name) like  '%' || ?1 || '%' "
+			+ "or lower(b.organization_name) like  '%' || ?1 || '%' "
+			+ "or lower(b.street) like  '%' || ?1 || '%'", nativeQuery=true)
 	List<Affiliate> findByTextQuery(String q);
 }
