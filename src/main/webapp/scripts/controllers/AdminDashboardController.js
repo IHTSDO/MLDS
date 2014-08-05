2f68d75a3e2f7d9b488a15aa27ae2dd0965c6ea1
David Jones <david@dgjones.info>
2014-08-05 15:05:07 -0400

    Use placeholder content for admin dashboard

--- src/main/webapp/scripts/controllers/AdminDashboardController.js
+++ src/main/webapp/scripts/controllers/AdminDashboardController.js
@@ -1,31 +1,8 @@
 'use strict';
 
 mldsApp.controller('AdminDashboardController',
-        [ '$scope', '$log', '$location', 'UserRegistrationService','PackagesService', 
-          function ($scope, $log, $location, UserRegistrationService, PackagesService) {
-        	$log.log('packages', PackagesService.query);
-        	
-        	$log.log('Dashboard Controller');
-        	
-        	$scope.applications = {};
-        	
-        	function getApplications() {
-        		var queryPromise =  UserRegistrationService.getApplications();
-        		
-        		queryPromise.success(function(data) {
-        			$scope.applications = data;
-        		});
-        	}
-        	
-        	getApplications();
-        	
-        	$scope.approveApplication = function(username){
-        		var queryPromise = UserRegistrationService.approveApplication(username);
-        		
-        		queryPromise.then(function(){
-        			getApplications();
-        		});
-        	};
-        	
+        [ '$scope', '$log', '$location', 
+          function ($scope, $log, $location) {
+        	        	
         }
     ]);
