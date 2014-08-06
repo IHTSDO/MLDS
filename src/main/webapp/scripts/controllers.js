bd119026711f7f1a4bfdab0d2614a3f3b05b2d46
David Jones <david@dgjones.info>
2014-08-06 13:18:53 -0400

    Fix validition messages not being shown on change password

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -41,22 +41,20 @@ mldsApp.controller('PasswordController', ['$scope', 'Password',
     function ($scope, Password) {
         $scope.success = null;
         $scope.error = null;
-        $scope.doNotMatch = null;
         $scope.changePassword = function () {
-            if ($scope.password != $scope.confirmPassword) {
-                $scope.doNotMatch = "ERROR";
-            } else {
-                $scope.doNotMatch = null;
-                Password.save($scope.password,
-                    function (value, responseHeaders) {
-                        $scope.error = null;
-                        $scope.success = 'OK';
-                    },
-                    function (httpResponse) {
-                        $scope.success = null;
-                        $scope.error = "ERROR";
-                    });
-            }
+    		if ($scope.form.$invalid) {
+    			$scope.form.attempted = true;
+    			return;
+    		}
+            Password.save($scope.password,
+                function (value, responseHeaders) {
+                    $scope.error = null;
+                    $scope.success = 'OK';
+                },
+                function (httpResponse) {
+                    $scope.success = null;
+                    $scope.error = "ERROR";
+                });
         };
     }]);
 
