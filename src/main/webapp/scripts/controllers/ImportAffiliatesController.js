f6d83a8dd46d939d87780c0212763b433129e35b
David Jones <david@dgjones.info>
2014-08-08 13:21:53 -0400

    MLDS-325 MLDS-446 MLDS-447 Connect upload to basic import endpoint

--- src/main/webapp/scripts/controllers/ImportAffiliatesController.js
+++ src/main/webapp/scripts/controllers/ImportAffiliatesController.js
@@ -1,8 +1,25 @@
 'use strict';
 
 mldsApp.controller('ImportAffiliatesController',
-        [ '$scope', '$log', '$location', 
-          function ($scope, $log, $location) {
-        	        	
+        [ '$scope', '$log', '$location', 'ImportAffiliatesService',
+          function ($scope, $log, $location, ImportAffiliatesService) {
+        	$scope.submitting = false;
+        	$scope.alerts = [];
+        	
+        	$scope.uploadFile = function(){
+        		$scope.alerts = [];
+                var file = $scope.affiliatesFile;
+                console.log('file is ' + JSON.stringify(file));
+                ImportAffiliatesService.importAffiliates(file)
+                	.then(function(result) {
+                		$scope.alerts.push({type: 'success', msg: 'Import completed.'});
+                		$scope.submitting = false;
+                	})
+        			["catch"](function(message) {
+        				$log.log(message);
+        				$scope.alerts.push({type: 'danger', msg: 'Network failure, please try again later. ['+ message.statusText+']'});
+        				$scope.submitting = false;
+        			});
+            };
         }
     ]);
