eac20d425b9dcf2d82e4af16ce5918f9bf3de135
David Jones <david@dgjones.info>
2014-08-08 22:55:20 -0400

    Start collecting validation errors

--- src/main/webapp/scripts/controllers/ImportAffiliatesController.js
+++ src/main/webapp/scripts/controllers/ImportAffiliatesController.js
@@ -5,18 +5,22 @@ mldsApp.controller('ImportAffiliatesController',
           function ($scope, $log, $location, ImportAffiliatesService) {
         	$scope.submitting = false;
         	$scope.alerts = [];
+        	$scope.importResult = null;
         	
         	$scope.uploadFile = function(){
         		$scope.alerts = [];
+        		$scope.importResult = null;
                 var file = $scope.affiliatesFile;
                 console.log('file is ' + JSON.stringify(file));
                 ImportAffiliatesService.importAffiliates(file)
                 	.then(function(result) {
+                		$scope.importResult = result.data;
                 		$scope.alerts.push({type: 'success', msg: 'Import completed.'});
                 		$scope.submitting = false;
                 	})
         			["catch"](function(message) {
         				$log.log(message);
+        				$scope.importResult = message.data;
         				$scope.alerts.push({type: 'danger', msg: 'Network failure, please try again later. ['+ message.statusText+']'});
         				$scope.submitting = false;
         			});
