af03a9b9fb33edced033fb7eaafa88af7cc1181b
anson-chung <anson.chung@intelliware.com>
2014-08-15 14:41:56 -0400

    MLDS-308 creating a get for the member license file

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/MemberRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package ca.intelliware.ihtsdo.mlds.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ca.intelliware.ihtsdo.mlds.domain.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	public Member findOneByKey(String key);
 }
