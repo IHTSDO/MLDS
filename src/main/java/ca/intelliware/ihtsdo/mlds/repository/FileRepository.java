cbb7e9e53c82a6cdcd54e4f8be0705a220ab5a72
anson-chung <anson.chung@intelliware.com>
2014-08-18 14:49:35 -0400

    MLDS-309 handling multipart post and storing blob in database

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/FileRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/FileRepository.java
@@ -3,10 +3,9 @@ package ca.intelliware.ihtsdo.mlds.repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import ca.intelliware.ihtsdo.mlds.domain.LicenseFile;
+import ca.intelliware.ihtsdo.mlds.domain.File;
 
 
 @Repository
-public interface LicenseFileRepository extends PagingAndSortingRepository<LicenseFile, Long>{
-		
+public interface FileRepository extends PagingAndSortingRepository<File, Long>{
 }
