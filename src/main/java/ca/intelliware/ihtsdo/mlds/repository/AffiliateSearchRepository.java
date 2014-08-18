535378a6acd3e3360aff1be7afc8fe94e3588938
Michael Buckley <michael.buckley@intelliware.com>
2014-08-18 15:26:31 -0400

    MLDS-312 Introducing paging to the full-text query

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/AffiliateSearchRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/AffiliateSearchRepository.java
@@ -10,29 +10,47 @@ import org.hibernate.search.jpa.FullTextEntityManager;
 import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
 import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
+import ca.intelliware.ihtsdo.mlds.domain.Member;
 
 @Service
 public class AffiliateSearchRepository {
 	@Resource
 	EntityManager entityManager;
 
-	public List<Affiliate> findAffiliatesMatching(String q) {
+	public List<Affiliate> findAffiliatesMatching(String q, Member homeMember, Pageable pageable) {
 		
 		FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
 	
 		QueryBuilder queryBuilder = fullTextEntityManager.getSearchFactory().buildQueryBuilder()
 				.forEntity(Affiliate.class).get();
 		
-		Query query = queryBuilder
-				.keyword()
-				.onField("ALL")
-				.matching(q)
+		Query query;
+		Query textQuery = queryBuilder.keyword()
+				.onField("ALL").matching(q)
 				.createQuery();
+		if (homeMember !=null) {
+			Query homeMemberQuery = queryBuilder.keyword()
+					.onField("homeMember").matching(homeMember.getKey())
+					.createQuery();
+			
+			query = queryBuilder
+					.bool()
+					.should(textQuery)
+					.must(homeMemberQuery)
+					.createQuery();
+		} else {
+			query = textQuery;
+		}
 		
 		FullTextQuery ftQuery = fullTextEntityManager.createFullTextQuery(query, Affiliate.class);
+		
+		// FIXME 0 based?
+		ftQuery.setFirstResult((pageable.getPageNumber() * pageable.getPageSize()) + pageable.getOffset());
+		ftQuery.setMaxResults(pageable.getPageSize());
 		
 		List<Affiliate> resultList = ftQuery.getResultList();
 		
