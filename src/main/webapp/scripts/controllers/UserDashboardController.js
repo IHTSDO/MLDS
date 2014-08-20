6ce56ddbd9c03dc602d75d02d943804f367d3e65
anson-chung <anson.chung@intelliware.com>
2014-08-20 09:51:02 -0400

    MLDS-309 adding view license button onto affiliate dashboard

--- src/main/webapp/scripts/controllers/UserDashboardController.js
+++ src/main/webapp/scripts/controllers/UserDashboardController.js
@@ -14,7 +14,10 @@ angular.module('MLDS')
         	$scope.approvedReleasePackagesByMember = [];
         	$scope.notApprovedReleasePackagesByMember = [];
 
-
+        	$scope.viewLicense = function (memberKey) {
+        		MemberService.getMemberLicense(memberKey);
+        	};
+        	
         	UserAffiliateService.promise.then(function() {
         		if (ApplicationUtilsService.isApplicationWaitingForApplicant(UserAffiliateService.affiliate.application)) {
         			$location.path('/affiliateRegistration');
