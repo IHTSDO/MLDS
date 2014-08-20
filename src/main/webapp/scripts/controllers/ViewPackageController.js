9b93e7f100a4c759d4a247f2e8c76be1d9652b35
anson-chung <anson.chung@intelliware.com>
2014-08-20 13:16:19 -0400

    MLDS-309 once primary is approved ihtsdo packages will be avaliable

--- src/main/webapp/scripts/controllers/ViewPackageController.js
+++ src/main/webapp/scripts/controllers/ViewPackageController.js
@@ -35,6 +35,7 @@ angular.module('MLDS')
 	};
 	
 	$scope.isPrimaryApplicationApproved = false;
+	$scope.isIHTSDOPackage = false;
 	$scope.isApplicationWaitingForApplicant = false;
 	$scope.matchingExtensionApplication = {};
 	
@@ -52,6 +53,7 @@ angular.module('MLDS')
 			$scope.isPrimaryApplicationApproved = ApplicationUtilsService.isApplicationApproved(UserAffiliateService.affiliate.application);
 			$scope.matchingExtensionApplication = getLatestMatchingMemberApplication(releasePackage);
 			$scope.isApplicationWaitingForApplicant = ApplicationUtilsService.isApplicationWaitingForApplicant($scope.matchingExtensionApplication);
+			$scope.isIHTSDOPackage = MemberService.isIhtsdoMember(releasePackage.member);
 		});
 	};
 
