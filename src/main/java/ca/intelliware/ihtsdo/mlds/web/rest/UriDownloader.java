477c7bbb103ce85b56b96a5677c605ca450962cc
David Jones <david@dgjones.info>
2014-08-20 11:42:59 -0400

    MLDS-296 MLDS-495 Switch to commons http directly and generate missing content-disposition

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/UriDownloader.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/UriDownloader.java
@@ -7,57 +7,85 @@ import java.net.URISyntaxException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UriDownloader {
 
+    private final Logger log = LoggerFactory.getLogger(UriDownloader.class);
+
 	public void download(String downloadUrl, HttpServletRequest clientRequest, HttpServletResponse clientResponse) {
 		try {
-	    	ClientHttpRequest hostingRequest = createHttpRequestFactory().createRequest(new URI(downloadUrl), HttpMethod.GET);
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			HttpGet hostingRequest = new HttpGet(downloadUrl);
 	    	copyClientHeadersToHostingRequest(clientRequest, hostingRequest);
-	    	ClientHttpResponse hostingResponse = hostingRequest.execute();
+	    	CloseableHttpResponse hostingResponse = httpclient.execute(hostingRequest);
 	    	try {
+	    		log.info("entity url="+downloadUrl+" disp="+hostingResponse.getFirstHeader("Content-Disposition")+" type="+hostingResponse.getFirstHeader("Content-Type"));
 		    	copyHostingHeadersToClientResponse(hostingResponse, clientResponse);
-		    	IOUtils.copyLarge(hostingResponse.getBody(), clientResponse.getOutputStream());
+		    	setContentDispositionIfMissing(hostingResponse, clientResponse, downloadUrl);
+		    	HttpEntity hostingEntity = hostingResponse.getEntity();
+		    	log.info("entity streaming="+hostingEntity.isStreaming());
+		    	hostingEntity.writeTo(clientResponse.getOutputStream());
+//		    	IOUtils.copyLarge(hostingEntity.getContent(), clientResponse.getOutputStream());
 	    	} finally {
-	    		//FIXME should we really be closing at this stage?
 	    		hostingResponse.close();
-	    		//FIXME do we need to destroy the httpRequestFactory?
+	    		log.info("entity download done");
 	    	}
 		} catch (IOException ex) {
 			throw new RuntimeException("Error while downloading file");
-		} catch (URISyntaxException ex) {
-			throw new RuntimeException("Error while downloading file (URI issue)");
 		}
 	}
 
-	private void copyClientHeadersToHostingRequest(HttpServletRequest clientRequest, ClientHttpRequest httpRequest) {
+	private void setContentDispositionIfMissing(CloseableHttpResponse hostingResponse, HttpServletResponse clientResponse, String downloadUrl) {
+		if (hostingResponse.getFirstHeader("Content-Disposition") != null) {
+			return;
+		}
+		try {
+			URI uri = new URI(downloadUrl);
+			String value = "attachment";
+			String path = uri.getPath();
+			if (path != null) {
+				int lastIndex = path.lastIndexOf('/');
+				if (lastIndex != -1) {
+					path = path.substring(lastIndex+1);
+				}
+				value += "; filename=\""+path+"\"";
+			}
+			clientResponse.addHeader("Content-Disposition", value);
+		} catch (URISyntaxException e) {
+			log.error("Failed to generate content-disposition due to error parsing uri");
+		}
+		
+	}
+
+	private void copyClientHeadersToHostingRequest(HttpServletRequest clientRequest, HttpGet hostingRequest) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private void copyHostingHeadersToClientResponse(ClientHttpResponse hostingResponse, HttpServletResponse clientResponse) {
-		HttpHeaders hostingHeaders = hostingResponse.getHeaders();
-		
-		if (hostingHeaders.getContentType() != null) {
-			clientResponse.setContentType(hostingHeaders.getContentType().toString());
-		}
-		if (hostingHeaders.getContentLength() != -1) {
-			//FIXME is there a long content length rather than int casting?
-			clientResponse.setContentLength((int) hostingHeaders.getContentLength());
-		}
+	private void copyHostingHeadersToClientResponse(CloseableHttpResponse hostingResponse, HttpServletResponse clientResponse) {
+		copyHostingHeaderToClientResponse("Content-Disposition", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("Content-Type", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("Content-Length", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("Date", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("ETag", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("Expires", hostingResponse, clientResponse);
+		copyHostingHeaderToClientResponse("Last-Modified", hostingResponse, clientResponse);
 	}
 
-	private HttpComponentsClientHttpRequestFactory createHttpRequestFactory() {
-		//FIXME how should we really be finding/configuring HttpComponentsClientHttpRequestFactory?
-		return new HttpComponentsClientHttpRequestFactory();
+	private void copyHostingHeaderToClientResponse(String key, CloseableHttpResponse hostingResponse, HttpServletResponse clientResponse) {
+		Header header = hostingResponse.getFirstHeader(key);
+		if (header != null) {
+			clientResponse.setHeader(key, header.getValue());
+		}
 	}
-
 }
