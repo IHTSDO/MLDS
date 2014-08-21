7d0cb71f66d8d19dff350cb306748f3267ec6653
anson-chung <anson.chung@intelliware.com>
2014-08-21 10:32:58 -0400

    MLDS-494 Creating affiliate summary page for staff users and linking to edit contact info page if user has permissions

---
+++ src/main/webapp/scripts/controllers/AffiliateSummaryController.js
@@ -0,0 +1,58 @@
+'use strict';
+
+mldsApp.controller('AffiliateSummaryController', [
+		'$scope',
+		'$log',
+		'$location',
+		'$modal',
+		'$routeParams',
+		'Session',
+		'AffiliateService',
+		'ApplicationUtilsService',
+		function($scope, $log, $location, $modal, $routeParams, Session, AffiliateService, ApplicationUtilsService) {
+			
+			var affiliateId = $routeParams.affiliateId && parseInt($routeParams.affiliateId, 10);
+			
+			$scope.alerts = [];
+			$scope.affiliate = {};
+			$scope.affiliateDetails = {};
+			$scope.approved = false;
+			$scope.isApplicationApproved = ApplicationUtilsService.isApplicationApproved;
+
+			function loadAffiliate() {
+				var queryPromise = AffiliateService.affiliate(affiliateId);
+				
+				queryPromise.success(function(affiliate) {
+					$scope.affiliate = affiliate;
+					$scope.approved = $scope.isApplicationApproved(affiliate.application);
+					$scope.isEditable = Session.isAdmin() || (Session.member.key == affiliate.application.member.key);
+					
+					$scope.affiliateDetails = (affiliate.affiliateDetails) ? affiliate.affiliateDetails : affiliate.application.affiliateDetails;
+				});
+					
+			}
+
+			loadAffiliate(); 
+			
+			$scope.editAffiliate = function editAffiliate() {
+				$location.path('/affiliates/' + affiliateId + '/edit')
+			};
+			
+			$scope.viewApplication = function viewApplication(application) {
+        		var modalInstance = $modal.open({
+        			templateUrl: 'views/admin/applicationSummaryModal.html',
+        			controller: 'ApplicationSummaryModalController',
+        			size:'lg',
+        			resolve: {
+        				application: function() {
+        					return application;
+        				}
+        			}
+        		});
+			};
+			
+			$scope.approveApplication = function approveApplication(application) {
+				$location.path('/applicationReview/' + application.applicationId);
+			};
+			
+		} ]);
