61d1b36f010ff9d18b711dbde6bbeaa46d35b474
Michael Buckley <michael.buckley@intelliware.com>
2014-08-28 16:49:25 -0400

    MLDS-329 Configure the Jackson ObjectMapper in our (non Spring integration) mock tests

---
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/MockMvcJacksonTestSupport.java
@@ -0,0 +1,44 @@
+package ca.intelliware.ihtsdo.mlds.web.rest;
+
+import java.util.List;
+
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import ca.intellware.ihtsdo.mlds.domain.json.ObjectMapperTestBuilder;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+public class MockMvcJacksonTestSupport {
+    public static class WebMvcConfigurationSupportSpy extends WebMvcConfigurationSupport {
+    	
+    	public List<HttpMessageConverter<?>> getDefaultHttpMessageConverters() {
+			List<HttpMessageConverter<?>> messageConverters = Lists.newArrayList();
+			super.addDefaultHttpMessageConverters(messageConverters);
+			return messageConverters;
+		}
+    }
+    
+
+    public HttpMessageConverter<?>[] getConfiguredMessageConverters() {
+		List<HttpMessageConverter<?>> messageConverters = 
+				new WebMvcConfigurationSupportSpy().getDefaultHttpMessageConverters();
+		
+		findJacksonMapperAndReplaceObjectMapper(messageConverters);
+		
+		return messageConverters.toArray(new HttpMessageConverter[0]);
+	}
+
+
+	private void findJacksonMapperAndReplaceObjectMapper(List<HttpMessageConverter<?>> messageConverters) {
+		MappingJackson2HttpMessageConverter jacksonMapper = 
+				(MappingJackson2HttpMessageConverter) Iterables.find(
+						messageConverters, 
+						Predicates.instanceOf(MappingJackson2HttpMessageConverter.class));
+		
+		jacksonMapper.setObjectMapper(new ObjectMapperTestBuilder().buildObjectMapper());
+	}
+}
