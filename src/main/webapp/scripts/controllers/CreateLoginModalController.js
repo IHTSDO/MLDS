52fc1f79479ca741add9b5dc19702e939aea4bb4
anson-chung <anson.chung@intelliware.com>
2014-08-28 16:16:44 -0400

    MLDS-491 Displaying modal with error/reason with input for new email for affiliate

---
+++ src/main/webapp/scripts/controllers/CreateLoginModalController.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('MLDS').controller('CreateLoginModalController',
+		['$scope', '$log', '$modalInstance', 'affiliate', 'reason',
+    function ($scope, $log, $modalInstance, affiliate, reason) {
+		$log.log('create login modal controller!', reason);
+		$scope.affiliate = affiliate;
+		
+		$scope.alerts = [];
+		
+		if (reason.noEmail) {
+			$scope.alerts.push({type: 'danger', msg: 'There is currently no email assigned to the affiliate.'});
+		}
+		
+		if (reason.duplicateEmail) {
+			$scope.alerts.push({type: 'danger', msg: 'There is already another affiliate with the same email.'});
+		}
+		
+		$scope.oldEmail = $scope.affiliate.affiliateDetails.email;
+		
+		$scope.ok = function() {
+			$scope.submitting = true;
+		};
+		
+    }]);
+
+
+
