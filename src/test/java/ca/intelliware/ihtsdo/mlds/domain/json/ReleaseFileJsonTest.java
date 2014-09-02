1f1b6d385babccc74bb80846fdf38844a3c1c65a
Michael Buckley <michael.buckley@intelliware.com>
2014-09-02 12:30:24 -0400

    MLDS-534 Fix broken json config.

---
+++ src/test/java/ca/intelliware/ihtsdo/mlds/domain/json/ReleaseFileJsonTest.java
@@ -0,0 +1,69 @@
+package ca.intelliware.ihtsdo.mlds.domain.json;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ca.intelliware.ihtsdo.mlds.domain.ReleaseFile;
+import ca.intelliware.ihtsdo.mlds.domain.ReleasePackage;
+import ca.intelliware.ihtsdo.mlds.domain.ReleaseVersion;
+import ca.intelliware.ihtsdo.mlds.security.SecurityContextSetup;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ReleaseFileJsonTest {
+	SecurityContextSetup securityContextSetup = new SecurityContextSetup();
+	ReleaseFile releaseFile;
+	ObjectMapper objectMapper;
+
+	@Before
+	public void configureJackson() {
+		objectMapper = new ObjectMapperTestBuilder().buildObjectMapper();
+	}
+	
+	@Before
+	public void buildObjectGraph() {
+		releaseFile = new ReleaseFile(55L);
+		ReleasePackage releasePackage = new ReleasePackage(22L);
+		ReleaseVersion releaseVersion = new ReleaseVersion(33L);
+		
+		releasePackage.addReleaseVersion(releaseVersion);
+		releaseVersion.addReleaseFile(releaseFile);
+	}
+	
+	@Test
+	public void adminSeesRawDownloadUrl() throws Exception {
+		securityContextSetup.asAdmin();
+		releaseFile.setDownloadUrl("http://example.com/file.zip");
+		
+		JsonNode tree = objectMapper.readTree(objectMapper.writeValueAsString(releaseFile));
+		
+		String downloadUrl = tree.get("downloadUrl").asText();
+		assertEquals("http://example.com/file.zip", downloadUrl);
+	}
+	
+	@Test
+	public void affiliateDoesNotSeeRawDownloadUrl() throws Exception {
+		securityContextSetup.asAffiliateUser();
+		releaseFile.setDownloadUrl("http://example.com/file.zip");
+		
+		JsonNode tree = objectMapper.readTree(objectMapper.writeValueAsString(releaseFile));
+		
+		assertNull("downloadUrl missing from tree", tree.get("downloadUrl"));
+	}
+	
+	@Test
+	public void affiliateDoesSeeProxiedDownloadUrl() throws Exception {
+		securityContextSetup.asAffiliateUser();
+		releaseFile.setDownloadUrl("http://example.com/file.zip");
+		
+		JsonNode tree = objectMapper.readTree(objectMapper.writeValueAsString(releaseFile));
+		
+		assertThat("clientDownloadUrl contains releaseFileId", tree.get("clientDownloadUrl").asText(), containsString("releaseFiles/"+releaseFile.getReleaseFileId()));
+	}
+}
