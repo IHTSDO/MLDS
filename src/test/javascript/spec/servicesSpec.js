d35188610960680be863e0889e063b6f0b8b486a
David Jones <david@dgjones.info>
2014-09-08 12:34:04 -0400

    MLDS-322 MLDS-606 Include basic audit service tests

--- src/test/javascript/spec/servicesSpec.js
+++ src/test/javascript/spec/servicesSpec.js
@@ -43,6 +43,91 @@ describe('Services Tests ', function () {
         });
 
     });
+
+
+    describe('AuditsService', function () {
+        var serviceTested,
+            httpBackend;
+
+        beforeEach(inject(function($httpBackend, AuditsService, authService) {
+            serviceTested = AuditsService;
+            httpBackend = $httpBackend;
+            //Request on app init
+            httpBackend.expectGET('i18n/en.json').respond(200, '');
+        }));
+        //make sure no expectations were missed in your tests.
+        //(e.g. expectGET or expectPOST)
+        afterEach(function() {
+            httpBackend.verifyNoOutstandingExpectation();
+            httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should call backend on findAll', function(){
+            //GIVEN
+            //set up some data for the http call to return and test later.
+            var returnData = { result: 'ok' };
+            //expectGET to make sure this is called once.
+            httpBackend.expectGET('/app/rest/audits').respond(returnData);        
+
+             //WHEN
+            serviceTested.findAll();
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            httpBackend.flush();
+        });
+
+        it('should call backend on findByDates with filter', function(){
+            //GIVEN
+            //set up some data for the http call to return and test later.
+            var returnData = { result: 'ok' };
+            //expectGET to make sure this is called once.
+            httpBackend.expectGET('/app/rest/audits?$filter=auditEventDate%20ge%20\'2014-01-01\'%20and%20auditEventDate%20le%20\'2014-01-09\'').respond(returnData);        
+
+             //WHEN
+            serviceTested.findByDates('2014-01-01', '2014-01-09');
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            httpBackend.flush();
+        });
+
+        it('should call backend on findByAuditEventType with filter', function(){
+            //GIVEN
+            //set up some data for the http call to return and test later.
+            var returnData = { result: 'ok' };
+            //expectGET to make sure this is called once.
+            httpBackend.expectGET('/app/rest/audits?$filter=auditEventType%20eq%20\'TestType\'').respond(returnData);        
+
+             //WHEN
+            serviceTested.findByAuditEventType('TestType');
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            httpBackend.flush();
+        });
+
+        it('should call backend on findByAffiliateId with filter', function(){
+            //GIVEN
+            //set up some data for the http call to return and test later.
+            var returnData = { result: 'ok' };
+            //expectGET to make sure this is called once.
+            httpBackend.expectGET('/app/rest/audits?$filter=affiliateId%20eq%20\'123\'').respond(returnData);        
+
+             //WHEN
+            serviceTested.findByAffiliateId(123);
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            httpBackend.flush();
+        });
+
+        it('should call backend on findByApplicationId with filter', function(){
+            //GIVEN
+            //set up some data for the http call to return and test later.
+            var returnData = { result: 'ok' };
+            //expectGET to make sure this is called once.
+            httpBackend.expectGET('/app/rest/audits?$filter=applicationId%20eq%20\'123\'').respond(returnData);        
+
+             //WHEN
+            serviceTested.findByApplicationId(123);
+            //flush the backend to "execute" the request to do the expectedGET assertion.
+            httpBackend.flush();
+        });
+
+    });
 });
 
 
