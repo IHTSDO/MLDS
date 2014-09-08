78367b8ae77395c9de5dd9fc3e80f22155049e6f
David Jones <david@dgjones.info>
2014-09-08 17:09:54 -0400

    MLDS-314 MLDS-608 Add new standing to affiliate and migrate

---
+++ src/main/java/ca/intelliware/ihtsdo/mlds/domain/StandingState.java
@@ -0,0 +1,14 @@
+package ca.intelliware.ihtsdo.mlds.domain;
+
+public enum StandingState {
+	APPLYING,
+	IN_GOOD_STANDING,
+	
+	REJECTED,
+	
+	DEACTIVATION_PENDING,
+	DEACTIVATED,
+	
+	DEREGISTRATION_PENDING,
+	DEREGISTERED
+}
