29f7c399837ca705c31e5b60b773b1008a2070f1
Michael Buckley <michael.buckley@intelliware.com>
2014-09-09 15:43:21 -0400

    MLDS-170 Show spinner during login call.

--- src/main/webapp/scripts/controllers/LoginController.js
+++ src/main/webapp/scripts/controllers/LoginController.js
@@ -3,11 +3,17 @@
 mldsApp.controller('LoginController', ['$scope', '$location', 'AuthenticationSharedService',
     function ($scope, $location, AuthenticationSharedService) {
         $scope.rememberMe = true;
+    	$scope.submitting = false;
         $scope.login = function () {
+        	$scope.submitting = true;
+        	console.log('submitting');
             AuthenticationSharedService.login({
                 username: $scope.username,
                 password: $scope.password,
                 rememberMe: $scope.rememberMe
+            })['finally'](function() {
+            	$scope.submitting = false;
+            	console.log('submitting done');
             });
         };
     }]);
