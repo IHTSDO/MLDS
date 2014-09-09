02b292bd567662ef0afb144046cb1b758b9c828f
Michael Buckley <michael.buckley@intelliware.com>
2014-09-09 11:50:46 -0400

    MLDS-170 Change access pattern -- db first, then look to remote.  Allows us to run of local "ADMIN" users during transition. - Extract PostRequestBuilder - Better check for no-user-found from remote web auth

--- src/main/java/ca/intelliware/ihtsdo/mlds/security/ihtsdo/HttpAuthAdaptor.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/security/ihtsdo/HttpAuthAdaptor.java
@@ -5,24 +5,29 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
-import org.apache.http.NameValuePair;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Charsets;
-import com.google.common.collect.Lists;
 import com.google.common.io.Closeables;
 
+/**
+ * HTTP query marshaller for the IHTSDO shared web authentication service.
+ */
 @Service
 public class HttpAuthAdaptor {
+	static final String PARAM_USERNAME = "username";
+	static final String PARAM_QUERY_NAME = "queryName";
+	static final String PARAM_APP_NAME = "appName";
+	static final String PARAM_PASSWORD = "password";
 
 	//FIXME MLDS-170 MB extract to config
 	String queryUrl = "https://usermanagement.ihtsdotools.org/security-web/query/";
@@ -40,48 +45,11 @@ public class HttpAuthAdaptor {
 		queryUrl = url;
 	}
 
-	static class PostRequestBuilder {
-		final HttpPost request;
-		List<NameValuePair> nameValuePairs = Lists.newArrayList();
-
-		public PostRequestBuilder(String queryUrl) {
-			request = new HttpPost(queryUrl);
-			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10 * 1000).build();
-			request.setConfig(requestConfig);
-		}
-		
-		public PostRequestBuilder addQueryName(String queryName) {
-			nameValuePairs.add(new BasicNameValuePair("queryName", queryName));
-			return this;
-		}
-
-		public void addUsername(String username) {
-			nameValuePairs.add(new BasicNameValuePair("username", username));
-		}
-
-		public void addPassword(String password) {
-			addParam("password", password);
-		}
-
-		public void addAppName(String appName) {
-			addParam("appName", appName);
-		}
-		
-		public void addParam(String paramName, String paramValue) {
-			nameValuePairs.add(new BasicNameValuePair(paramName, paramValue));
-		}
-
-		public HttpPost toRequest() {
-			request.setEntity(new UrlEncodedFormEntity(nameValuePairs, Charsets.ISO_8859_1));
-			return request;
-		}
-	}
-
 	public boolean checkUsernameAndPasswordValid(String username, String password) throws IOException, IllegalStateException, ClientProtocolException {
 		PostRequestBuilder builder = new PostRequestBuilder(queryUrl);
-		builder.addQueryName("getUserByNameAuth");
-		builder.addUsername(username);
-		builder.addPassword(password);
+		builder.addParam(PARAM_QUERY_NAME, "getUserByNameAuth");
+		builder.addParam(PARAM_USERNAME, username);
+		builder.addParam(PARAM_PASSWORD, password);
 		HttpPost request = builder.toRequest();
 	    
 		CloseableHttpResponse response = httpClient.execute(request);
@@ -101,15 +69,27 @@ public class HttpAuthAdaptor {
 		return result;
 	}
 
-	public CentralAuthUserInfo getUserInfo(String username) throws ClientProtocolException, IOException {
+	public CentralAuthUserInfo getUserInfo(String username) throws IOException {
 		PostRequestBuilder builder = new PostRequestBuilder(queryUrl);
-		builder.addQueryName("getUserByName");
-		builder.addUsername(username);
+		builder.addParam(PARAM_QUERY_NAME, "getUserByName");
+		builder.addParam(PARAM_USERNAME, username);
 		HttpPost request = builder.toRequest();
 	    
 		CloseableHttpResponse response = httpClient.execute(request);
 		
-		return objectMapper.readValue(response.getEntity().getContent(), CentralAuthUserInfo.class);
+		HttpEntity entity = new BufferedHttpEntity(response.getEntity());
+		
+		try {
+			return objectMapper.readValue(entity.getContent(), CentralAuthUserInfo.class);
+		} catch (JsonParseException e) {
+			// the system returns 200 "NO RESPONSE" instead of a 404 on no-user.  Return null
+			String body = EntityUtils.toString(entity);
+			if ("NO RESPONSE".equals(body)) {
+				return null;
+			} else {
+				throw new IllegalStateException("Unexpected JSON: " + body, e);
+			}
+		}
 	}
 	
 	static class AuthoritiesResponse {
@@ -126,10 +106,10 @@ public class HttpAuthAdaptor {
 	
 	public List<CentralAuthUserPermission> getUserPermissions(String username) throws ClientProtocolException, IOException {
 		PostRequestBuilder builder = new PostRequestBuilder(queryUrl);
-		builder.addQueryName("getUserAppPerms");
-		builder.addUsername(username);
+		builder.addParam(PARAM_QUERY_NAME, "getUserAppPerms");
+		builder.addParam(PARAM_USERNAME, username);
 		//FIXME MLDS-170 MB extract to config
-		builder.addAppName("MLDS");
+		builder.addParam(PARAM_APP_NAME, "MLDS");
 		HttpPost request = builder.toRequest();
 		
 		CloseableHttpResponse response = httpClient.execute(request);
