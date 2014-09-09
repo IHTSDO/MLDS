ca8c44a97c62881a31b6193afc8587adcf6c5bff
Michael Buckley <michael.buckley@intelliware.com>
2014-09-09 15:26:23 -0400

    Imports cleanup

--- src/test/java/ca/intelliware/ihtsdo/mlds/security/ihtsdo/HttpAuthAuthenticationProviderTest.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/security/ihtsdo/HttpAuthAuthenticationProviderTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.junit.Before;
