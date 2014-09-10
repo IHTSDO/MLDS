912f73f8b2cad579cd4d8b6a542f6645da3e5049
David Jones <david@dgjones.info>
2014-09-10 11:37:46 -0400

    MLDS-314 MLDS-616 Disallow downloads for deactivated users at rest level

--- src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/ReleasePackagesResource_ReleaseFiles_Test.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/ReleasePackagesResource_ReleaseFiles_Test.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -14,9 +16,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.util.NestedServletException;
 
 import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
 import ca.intelliware.ihtsdo.mlds.domain.ReleaseFile;
+import ca.intelliware.ihtsdo.mlds.domain.ReleaseVersion;
 import ca.intelliware.ihtsdo.mlds.repository.ReleaseFileRepository;
 import ca.intelliware.ihtsdo.mlds.repository.ReleasePackageRepository;
 import ca.intelliware.ihtsdo.mlds.repository.ReleaseVersionRepository;
@@ -101,4 +105,24 @@ public class ReleasePackagesResource_ReleaseFiles_Test {
 
 		Mockito.verify(releasePackageAuditEvents).logDownload(Mockito.any(ReleaseFile.class), Mockito.anyInt(), Mockito.any(Affiliate.class));
 	}
+	
+	@Test
+	public void downloadReleaseFileShouldFailIfCheckDenied() throws Exception {
+		ReleaseFile releaseFile = withReleaseFileWithIdOf(3L);
+		releaseFile.setDownloadUrl("http://test.com/file");
+		
+		Mockito.doThrow(new IllegalStateException("ACCOUNT DEACTIVATED")).when(authorizationChecker).checkCanDownloadReleaseVersion(Mockito.any(ReleaseVersion.class));
+
+		try {
+			restReleasePackagesResource.perform(MockMvcRequestBuilders.get(Routes.RELEASE_FILE_DOWNLOAD, 1L, 2L, 3L)
+					.contentType(MediaType.APPLICATION_JSON)
+	                .accept(MediaType.APPLICATION_JSON))
+	                .andExpect(status().is5xxServerError());
+        } catch (NestedServletException e) {
+        	Assert.assertThat(e.getRootCause().getMessage(), Matchers.containsString("ACCOUNT DEACTIVATED"));
+        }
+		
+		Mockito.verify(uriDownloader, Mockito.never()).download(Mockito.eq("http://test.com/file"), Mockito.any(HttpServletRequest.class), Mockito.any(HttpServletResponse.class));
+	}
+
 }
