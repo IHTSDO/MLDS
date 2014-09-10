a1a1c8ec435fd46f4522d7167e5d612d2a9ce3d3
David Jones <david@dgjones.info>
2014-09-10 14:36:16 -0400

    MLDS-314 MLDS-615 Block autlogin service for when user is deactivated

--- src/main/webapp/scripts/controllers/ActivationController.js
+++ src/main/webapp/scripts/controllers/ActivationController.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('MLDS').controller('ActivationController',
-		['$scope', '$routeParams', 'Activate', '$timeout', '$location', 'Session',
-    function ($scope, $routeParams, Activate, $timeout, $location, Session) {
+		['$rootScope', '$scope', '$routeParams', '$log', 'Activate', '$timeout', '$location', 'Session',
+    function ($rootScope, $scope, $routeParams, $log, Activate, $timeout, $location, Session) {
 		$rootScope.authenticationError = false;
 	    $rootScope.authenticated = false;
 	    $rootScope.account = null;
@@ -10,11 +10,13 @@ angular.module('MLDS').controller('ActivationController',
 	    
         Activate.get({key: $routeParams.key},
             function (value, responseHeaders) {
+        		$log.log('ok');
                 $scope.error = null;
                 $scope.success = 'OK';
                 $timeout(function() { $location.path('/dashboard'); }, 2000);
             },
             function (httpResponse) {
+            	$log.log('error');
                 $scope.success = null;
                 $scope.error = "ERROR";
             });
