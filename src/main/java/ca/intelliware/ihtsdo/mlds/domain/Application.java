35e40b7932c2756d09d114cffd5484b3afa33459
David Jones <david@dgjones.info>
2014-09-10 12:13:41 -0400

    MLDS-314 MLDS-610 Allow filtering for Applying affiliates and exclude original affiliate for review search

--- src/main/java/ca/intelliware/ihtsdo/mlds/domain/Application.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/domain/Application.java
@@ -34,7 +34,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 	@Type(value=ExtensionApplication.class, name="EXTENSION"),
 	@Type(value=ImportApplication.class, name="IMPORT")
 	})
-@Indexed
 public abstract class Application extends BaseEntity {
 	public static enum ApplicationType {
 		PRIMARY, EXTENSION, IMPORT
@@ -55,7 +54,7 @@ public abstract class Application extends BaseEntity {
 		
 	@OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="affiliate_details_id")
-	@IndexedEmbedded
+	@IndexedEmbedded(prefix="")
 	AffiliateDetails affiliateDetails;
 	
 	@Column(name="notes_internal")
