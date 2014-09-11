9957eccbe737a076ee7b86c8c0acaf2d82b6bbe9
David Jones <david@dgjones.info>
2014-09-11 12:08:26 -0400

    MLDS-314 MLDS-613 approve application rest endpoint testing

--- src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/ApplicationResource_CreateApplication_Test.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/ApplicationResource_CreateApplication_Test.java
@@ -3,6 +3,8 @@ package ca.intelliware.ihtsdo.mlds.web.rest;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,16 +20,26 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.util.NestedServletException;
 
+import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
+import ca.intelliware.ihtsdo.mlds.domain.AffiliateDetails;
 import ca.intelliware.ihtsdo.mlds.domain.Application.ApplicationType;
 import ca.intelliware.ihtsdo.mlds.domain.ExtensionApplication;
 import ca.intelliware.ihtsdo.mlds.domain.Member;
+import ca.intelliware.ihtsdo.mlds.domain.PrimaryApplication;
+import ca.intelliware.ihtsdo.mlds.domain.StandingState;
+import ca.intelliware.ihtsdo.mlds.repository.AffiliateDetailsRepository;
+import ca.intelliware.ihtsdo.mlds.repository.AffiliateRepository;
 import ca.intelliware.ihtsdo.mlds.repository.ApplicationRepository;
 import ca.intelliware.ihtsdo.mlds.repository.MemberRepository;
+import ca.intelliware.ihtsdo.mlds.repository.UserRepository;
+import ca.intelliware.ihtsdo.mlds.service.AffiliateAuditEvents;
+import ca.intelliware.ihtsdo.mlds.service.AffiliateDetailsResetter;
 import ca.intelliware.ihtsdo.mlds.service.ApplicationService;
+import ca.intelliware.ihtsdo.mlds.service.mail.ApplicationApprovedEmailSender;
 import ca.intelliware.ihtsdo.mlds.web.RouteLinkBuilder;
 import ca.intelliware.ihtsdo.mlds.web.rest.ApplicationResource.CreateApplicationDTO;
 
-public class ApplicationResourceTest {
+public class ApplicationResource_CreateApplication_Test {
 	private MockMvc mockMvc;
 	
 	@Mock
@@ -45,6 +57,7 @@ public class ApplicationResourceTest {
 	@Mock
 	ApplicationService applicationService;
 	
+	
 	ApplicationResource applicationResource;
 	
 	Member sweden;
@@ -60,6 +73,7 @@ public class ApplicationResourceTest {
         applicationResource.authorizationChecker = applicationAuthorizationChecker;
         applicationResource.applicationService = applicationService;
         applicationResource.memberRepository = memberRepository;
+        
         applicationResource.routeLinkBuilder = new RouteLinkBuilder();
 
         this.mockMvc = MockMvcBuilders.standaloneSetup(applicationResource).build();
@@ -123,4 +137,5 @@ public class ApplicationResourceTest {
 		ExtensionApplication application = new ExtensionApplication(applicationId);
 		Mockito.when(applicationService.startNewApplication(ApplicationType.EXTENSION, sweden)).thenReturn(application);
 	}
+	
 }
