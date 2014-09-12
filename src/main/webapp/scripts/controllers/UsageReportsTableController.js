87e34a65f4c68356bfc2be071cc951d11686ea8d
David Jones <david@dgjones.info>
2014-09-12 15:18:57 -0400

    MLDS-307 MLDS-660 Share reports table on the user dashboard and add more ng-init config to support optional columns

--- src/main/webapp/scripts/controllers/UsageReportsTableController.js
+++ src/main/webapp/scripts/controllers/UsageReportsTableController.js
@@ -6,8 +6,17 @@ mldsApp.controller('UsageReportsTableController', [
 		'UsageReportsService',
 		function($scope, $log, UsageReportsService) {
 			$scope.usageReportsUtils = UsageReportsService;
+			$scope.showAllColumns = true;
+			$scope.showViewAll = false;
 			
 			$scope.isActiveUsageReport = function(usageReport) {
 				return !!usageReport.effectiveTo;
+			};
+			
+			$scope.setShowAllColumns = function(set) {
+				$scope.showAllColumns = set;
+			};
+			$scope.setShowViewAll = function(set) {
+				$scope.showViewAll = set;
 			};
 		} ]);
