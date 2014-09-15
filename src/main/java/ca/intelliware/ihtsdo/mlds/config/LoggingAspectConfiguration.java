bbaf2feea2a8b245605238b4fd37bfa10c73785e
Michael Buckley <michael.buckley@intelliware.com>
2014-09-15 14:39:12 -0400

    disable noisy dev logging

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/LoggingAspectConfiguration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/LoggingAspectConfiguration.java
@@ -1,13 +1,12 @@
 package ca.intelliware.ihtsdo.mlds.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 
 import ca.intelliware.ihtsdo.mlds.aop.logging.LoggingAspect;
 
-@Configuration
+//@Configuration
 @EnableAspectJAutoProxy
 public class LoggingAspectConfiguration {
 
