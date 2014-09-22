440de9c3150e777a143be39967aedcddc3166c43
Michael Buckley <michael.buckley@intelliware.com>
2014-09-22 17:41:10 -0400

    MLDS-692 When admin or users land, redirect them to "pending applications" and "dashboard" respectively. Split MainController out to file

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -2,27 +2,6 @@
 
 /* Controllers */
 
-mldsApp.controller('MainController', ['$scope', '$rootScope', 'Session', '$log', '$location', 'AuthenticationSharedService',
-    function ($scope, $rootScope, Session, $log, $location, AuthenticationSharedService) {
-		// Used to reverse the result of a function in filters
-		$rootScope.not = function(func) {
-		    return function (item) { 
-		        return !func(item); 
-		    };
-		};
-		//$log.log(Session.promise);
-		$rootScope.Session = Session;
-
-//		Session.promise
-//			.then(function() {
-//				if (Session.isStaffOrAdmin()) {
-//					$location.path('/adminDashboard').replace();		
-//				} else if (Session.isUser()) {
-//					$location.path('/dashboard').replace();
-//				}
-//			});
-    }]);
-
 mldsApp.controller('AdminController', ['$scope',
     function ($scope) {
     }]);
