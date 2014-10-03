c5f7721c5d5e799c7729fd0ef156ed05d97e5ae6
Michael Buckley <michael.buckley@intelliware.com>
2014-10-03 17:49:28 -0400

    MLDS-635 Clean out test users from db

--- src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/UserResourceTest.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/UserResourceTest.java
@@ -46,15 +46,6 @@ public class UserResourceTest {
     }
 
     @Test
-    public void testGetExistingUser() throws Exception {
-        restUserMockMvc.perform(get("/app/rest/users/admin")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.lastName").value("Administrator"));
-    }
-
-    @Test
     public void testGetUnknownUser() throws Exception {
         restUserMockMvc.perform(get("/app/rest/users/unknown")
                 .accept(MediaType.APPLICATION_JSON))
