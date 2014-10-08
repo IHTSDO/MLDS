396ce76e26ca6210cd1b44a6faf3eb298b71fc95
Michael Buckley <michael.buckley@intelliware.com>
2014-10-08 16:43:51 -0400

    MLDS-734 Format

--- src/main/webapp/scripts/directives/inputTel.js
+++ src/main/webapp/scripts/directives/inputTel.js
@@ -1,23 +1,27 @@
 'use strict';
 
 angular.module('MLDS').directive('input', function() {
-    return {
-        restrict: 'E',
-        require: '?ngModel',
-        link: function (scope, element, attr, ngModel) {
-          if (attr.type !== 'tel') return;
+	return {
+		restrict : 'E',
+		require : '?ngModel',
+		link : function(scope, element, attr, ngModel) {
+			// Only apply to <input type="tel">
+			if (attr.type !== 'tel') {
+				return;
+			}
 
-          var read = function() {
-              var inputValue = element.val();
-              ngModel.$setViewValue(inputValue);
-            }      
-            element.intlTelInput({
-            	preferredCountries:[]
-            });
-            element.on('focus blur keyup change', function() {
-                scope.$apply(read);
-            });
-            read();          
-        }
-    };
+			element.intlTelInput({
+				preferredCountries : []
+			});
+
+			var copyInputToModel = function() {
+				var inputValue = element.val();
+				ngModel.$setViewValue(inputValue);
+			}
+			element.on('focus blur keyup change', function() {
+				scope.$apply(copyInputToModel);
+			});
+			copyInputToModel();
+		}
+	};
 });
