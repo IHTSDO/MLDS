3c69eee79447752919704ff1f7e6c2ee19fb39b9
Michael Buckley <michael.buckley@intelliware.com>
2014-10-09 11:07:17 -0400

    MLDS-734 Match country on initial render

--- src/main/webapp/scripts/directives/inputTel.js
+++ src/main/webapp/scripts/directives/inputTel.js
@@ -4,24 +4,42 @@ angular.module('MLDS').directive('input', function() {
 	return {
 		restrict : 'E',
 		require : '?ngModel',
+		priority: 1, // otherwise the input directive replaces our $render
 		link : function(scope, element, attr, ngModel) {
-			// Only apply to <input type="tel">
-			if (attr.type !== 'tel') {
+			// Only apply to <input type="tel"> and bound elements
+			if (attr.type !== 'tel' ||
+				!ngModel) {
 				return;
 			}
 
+			var telCountryModel = attr['telCountryModel'],
+				copyInputToModel;
+			
 			element.intlTelInput({
 				preferredCountries : []
 			});
-
-			var copyInputToModel = function() {
+			
+			ngModel.$render = function() {
+				element.intlTelInput("setNumber", ngModel.$viewValue||'');
+			};
+			
+			copyInputToModel = function() {
 				var inputValue = element.val();
 				ngModel.$setViewValue(inputValue);
 			}
+			
 			element.on('focus blur keyup change', function() {
 				scope.$apply(copyInputToModel);
 			});
-			copyInputToModel();
+			
+			if (telCountryModel) {
+				scope.$watch(telCountryModel, function(isoCode2){
+					// default the country if there is no content yet.
+					if (!element.val() && isoCode2) {
+						element.intlTelInput("selectCountry", isoCode2.toLowerCase());
+					}
+				});
+			}
 		}
 	};
 });
