17f387f2cdf0790ddabf3d192e9299157900f2fe
Michael Buckley <michael.buckley@intelliware.com>
2014-10-10 17:04:46 -0400

    Change Jackson serialization implementation from raw EntityManager use to MemberRepository for easier mocking.

--- src/main/java/ca/intelliware/ihtsdo/mlds/domain/json/MLDSJacksonModule.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/domain/json/MLDSJacksonModule.java
@@ -1,29 +1,28 @@
 package ca.intelliware.ihtsdo.mlds.domain.json;
 
-import javax.persistence.EntityManager;
-
 import ca.intelliware.ihtsdo.mlds.domain.Member;
 import ca.intelliware.ihtsdo.mlds.domain.MemberJsonDeserializer;
 import ca.intelliware.ihtsdo.mlds.domain.MemberSerializer;
 import ca.intelliware.ihtsdo.mlds.domain.ReleaseFile;
+import ca.intelliware.ihtsdo.mlds.repository.MemberRepository;
 import ca.intelliware.ihtsdo.mlds.security.ihtsdo.CurrentSecurityContext;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class MLDSJacksonModule extends SimpleModule {
-	final EntityManager em;
+	final MemberRepository memberRepository;
 	final transient CurrentSecurityContext currentSecurityContext;
 	private static final long serialVersionUID = 1L;
 
-	public MLDSJacksonModule(EntityManager em, CurrentSecurityContext securityConext) {
+	public MLDSJacksonModule(MemberRepository memberRepository, CurrentSecurityContext securityConext) {
 		super("MLDS Jackson");
-		this.em = em;
+		this.memberRepository = memberRepository;
 		this.currentSecurityContext = securityConext;
 		
 		addSerializer(ReleaseFile.class, new ReleaseFileSerializer());
 		
 		addSerializer(Member.class, new MemberSerializer());
 		
-		addDeserializer(Member.class, new MemberJsonDeserializer(em));
+		addDeserializer(Member.class, new MemberJsonDeserializer(memberRepository));
 	}
 }
