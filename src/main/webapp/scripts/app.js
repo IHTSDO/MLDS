2def3f608809706e191e55300e7a2f707ee50450
Michael Buckley <michael.buckley@intelliware.com>
2014-10-21 12:31:15 -0400

    MLDS-801 apply date picker to publishedAt date on ReleaseVersion

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -428,6 +428,12 @@ mldsApp
             //$httpProvider.responseInterceptors.push(delayHandlerFactory);
             
         }])
+        .run(['datepickerConfig', function(datepickerConfig){
+        	// change default date config to ISO8601 and Euro Monday weeks.
+        	datepickerConfig.format="yyyy-MM-dd";
+        	datepickerConfig.startingDay=1;
+        	datepickerConfig.formatYear='yy';
+        }])
         .run(['$rootScope', '$location', '$anchorScroll', '$routeParams', '$log', '$timeout',
               function($rootScope, $location, $anchorScroll, $routeParams,$log, $timeout) {
 		  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
--- src/main/webapp/scripts/controllers/AddEditReleaseVersionModalController.js
+++ src/main/webapp/scripts/controllers/AddEditReleaseVersionModalController.js
@@ -14,10 +14,29 @@ angular.module('MLDS').controller('AddEditReleaseVersionModalController',
 	$scope.submitting = false;
 	$scope.alerts = [];
 	
+	function serializeDate(date) {
+		if (date) {
+			return moment(date).format('YYYY-MM-DD');
+		} else {
+			return null;
+		}
+	};
+
+	$scope.dateOpen = {};
+	
+	$scope.openDate = function($event, name) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.dateOpen[name] = true;
+		$log.log('openDate scope', name, $scope.dateOpen[name], $scope);
+	}
+	
 	$scope.ok = function(form) {
 		$scope.submitAttempted = true;
 		$scope.submitting = true;
 		$scope.alerts.splice(0, $scope.alerts.length);
+		
+		$scope.releaseVersion.publishedAt = serializeDate($scope.releaseVersion.publishedAt);
 		
 		ReleaseVersionsService[isNewObject?'save':'update']({releasePackageId : releasePackage.releasePackageId}, $scope.releaseVersion)
 			.$promise.then(function(result) {
