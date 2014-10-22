5e372672d87c9e09b3dc99ae3f72ffba75179c3d
Michael Buckley <michael.buckley@intelliware.com>
2014-10-22 17:35:53 -0400

    MLDS-645 Add standing state as search term

---
+++ src/main/java/ca/intelliware/ihtsdo/mlds/search/StandingStateFieldBridge.java
@@ -0,0 +1,8 @@
+package ca.intelliware.ihtsdo.mlds.search;
+
+public class StandingStateFieldBridge extends TranslatedEnumFieldBridge {
+	@Override
+	protected String getBundleKeyPrefix() {
+		return "affiliate.standingState.";
+	}
+}
