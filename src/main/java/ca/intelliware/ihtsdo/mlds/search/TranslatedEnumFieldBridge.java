ff3f104a66393055d8f4461e8fd79850b0a08b78
Michael Buckley <michael.buckley@intelliware.com>
2014-10-27 13:47:18 -0400

    Debugging remote build errors.

--- src/main/java/ca/intelliware/ihtsdo/mlds/search/TranslatedEnumFieldBridge.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/search/TranslatedEnumFieldBridge.java
@@ -3,6 +3,8 @@ package ca.intelliware.ihtsdo.mlds.search;
 import java.util.Locale;
 
 import org.hibernate.search.bridge.StringBridge;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Dumb mapping of our enums to search, assuming English.
@@ -10,6 +12,8 @@ import org.hibernate.search.bridge.StringBridge;
  * Uses the client-side translations to generate search text.
  */
 abstract public class TranslatedEnumFieldBridge implements StringBridge {
+    final Logger log = LoggerFactory.getLogger(TranslatedEnumFieldBridge.class);
+
 	public TranslatedEnumFieldBridge() {
 	}
 	
@@ -18,6 +22,7 @@ abstract public class TranslatedEnumFieldBridge implements StringBridge {
 		if (object == null) {
 			return null;
 		}
+		log.error("objectToString() {} {}", object.getClass().getName(), object);
 		Enum<?> affiliateType = (Enum<?>) object;
 		String path = getBundleKeyPrefix() + affiliateType.name();
 		AngularTranslateService translateInstance = AngularTranslateService.getInstance();
