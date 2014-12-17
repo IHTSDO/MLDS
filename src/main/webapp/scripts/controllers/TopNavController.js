76c090286b38590eb8cc8a55c1329c1a1d55d755
David Jones <david@dgjones.info>
2014-12-17 13:08:07 -0500

    MLDS-738 investigate reduction of flicker on changing logo

--- src/main/webapp/scripts/controllers/TopNavController.js
+++ src/main/webapp/scripts/controllers/TopNavController.js
@@ -3,15 +3,15 @@
 angular.module('MLDS').controller('TopNavController',
 		['$rootScope', '$scope', '$routeParams', '$log', '$timeout', '$location', '$filter', 'Session', 'MemberService',
     function ($rootScope, $scope, $routeParams, $log, $timeout, $location, $filter, Session, MemberService) {
+		$scope.memberReady = false;
 		$scope.memberName = '';
 		$scope.memberLogo = '';
 		
 		$scope.$watch('Session.member', function(memberKey) {
-			$log.info('watch member', Session.member);
-			$scope.memberName = Session.member ? $filter('enum')(Session.member.key, 'global.member.') : '';
-			$scope.memberLogo = '';
 			if (Session.member) {
 				MemberService.ready.then(function() {
+					$scope.memberName = Session.member ? $filter('enum')(Session.member.key, 'global.member.') : '';
+					$scope.memberLogo = '';
 					var member = MemberService.membersByKey[Session.member.key];
 					if (member && member.name) {
 						$scope.memberName = member.name;
@@ -19,7 +19,12 @@ angular.module('MLDS').controller('TopNavController',
 					if (member && member.logo) {
 						$scope.memberLogo = MemberService.getMemberLogoUrl(member.key);
 					}
+					$scope.memberReady = true;
 				});
+			} else {
+				$scope.memberReady = false;
+				$scope.memberName = '';
+				$scope.memberLogo = '';
 			}
 		});
     }]);
