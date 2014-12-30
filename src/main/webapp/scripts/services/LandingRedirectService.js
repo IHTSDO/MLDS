b00916de0656a2c4a5e2b1762e2df64715f91394
Anson Chung <anson.chung@intelliware.ca>
2014-12-30 11:02:04 -0500

    Cleaning up logging statements

--- src/main/webapp/scripts/services/LandingRedirectService.js
+++ src/main/webapp/scripts/services/LandingRedirectService.js
@@ -3,14 +3,14 @@
 angular.module('MLDS').service('LandingRedirectService', 
 		['$log', '$location', '$route', 'UserAffiliateService', 'ApplicationUtilsService',
          function ($log, $location, $route, UserAffiliateService, ApplicationUtilsService) {
-			$log.log('LandingRedirectService starting', $location.path(), $route.current, window.location.hash);
+			//$log.log('LandingRedirectService starting', $location.path(), $route.current, window.location.hash);
 
 			var service = {}; 
 			
 			service.redirect = function(Session) {
 				Session.promise
 				.then(function() {
-					$log.log('LandingRedirectService redirecting', $location.path(), $route.current, window.location.hash);
+					//$log.log('LandingRedirectService redirecting', $location.path(), $route.current, window.location.hash);
 					if (Session.isStaffOrAdmin()) {
 						$location.path('/pendingApplications').replace();		
 					} else if (Session.isUser()) {
