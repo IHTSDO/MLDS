0f4b25c21078aa09dc89cdc81e5bce20592b7f79
Anson Chung <anson.chung@intelliware.ca>
2014-12-31 11:06:08 -0500

    Adding an All button got standing

--- src/main/webapp/scripts/controllers/LandingRedirectController.js
+++ src/main/webapp/scripts/controllers/LandingRedirectController.js
@@ -3,11 +3,11 @@
 angular.module('MLDS').controller('LandingRedirectController', 
 		['$scope', '$rootScope', 'Session', '$log', '$location', '$route', 'AuthenticationSharedService', 'UserAffiliateService', 'ApplicationUtilsService',
          function ($scope, $rootScope, Session, $log, $location, $route, AuthenticationSharedService, UserAffiliateService, ApplicationUtilsService) {
-			$log.log('LandingRedirectController starting', $location.path(), $route.current, window.location.hash)
+			//$log.log('LandingRedirectController starting', $location.path(), $route.current, window.location.hash)
 
       		Session.promise
       			.then(function() {
-      				$log.log('LandingRedirectController redirecting', $location.path(), $route.current, window.location.hash)
+      				//$log.log('LandingRedirectController redirecting', $location.path(), $route.current, window.location.hash)
       				if (Session.isStaffOrAdmin()) {
       					$location.path('/pendingApplications').replace();		
       				} else if (Session.isUser()) {
