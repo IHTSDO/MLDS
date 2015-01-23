4ffc468b057805d177aa4f982ffb35d2254a594a
Peter G. Williams <pwi@ihtsdo.org>
2015-01-23 10:19:38 +0000

    MLDS-895 Fix removing country also removing hospitals for that country in other usage reports.

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/CommercialUsageEntryRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/CommercialUsageEntryRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import ca.intelliware.ihtsdo.mlds.domain.CommercialUsage;
 import ca.intelliware.ihtsdo.mlds.domain.CommercialUsageEntry;
 import ca.intelliware.ihtsdo.mlds.domain.Country;
 
 public interface CommercialUsageEntryRepository extends JpaRepository<CommercialUsageEntry, Long> {
 
-	List<CommercialUsageEntry> findByCountry(Country country);
+	// List<CommercialUsageEntry> findByCountry(Country country);
+
+	List<CommercialUsageEntry> findByCountryAndCommercialUsage(Country country, CommercialUsage commercialUsage);
 
 }
