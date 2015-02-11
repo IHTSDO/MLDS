7b62bcbcddd33586adbb53e54487d738d906de83
Chris-Swires <csw@ihtsdo.org>
2015-02-11 06:37:23 +0000

    MLDS-900 Release Package page ordering - As well as the creation of an Accordion for the Member Country Releases.

--- src/main/webapp/scripts/controllers/ViewReleasesController.js
+++ src/main/webapp/scripts/controllers/ViewReleasesController.js
@@ -2,9 +2,8 @@
 
 angular.module('MLDS')
     .controller('ViewReleasesController', 
-    		['$scope', '$log', 'PackageUtilsService', '$location', 'MemberService', 'UserAffiliateService','releasePackagesQueryResult', 'MemberPackageService',
-           function ($scope, $log, PackageUtilsService, $location, MemberService, UserAffiliateService, releasePackagesQueryResult, MemberPackageService) {
-			
+    		['$scope', '$log', 'Session', 'PackageUtilsService', '$location', 'MemberService', 'UserAffiliateService','releasePackagesQueryResult', 'MemberPackageService',
+           function ($scope, $log, Session, PackageUtilsService, $location, MemberService, UserAffiliateService, releasePackagesQueryResult, MemberPackageService) {
 	$scope.utils = PackageUtilsService;
 	$scope.releasePackagesByMember = [];
 	$scope.alerts = [];
@@ -22,12 +21,34 @@ angular.module('MLDS')
 	$scope.releasePackageOrderBy = MemberPackageService.orderByJustName; 
 
 	var releasePackages = releasePackagesQueryResult;
-				$scope.releasePackagesByMember = _.chain(releasePackages)
-					.filter(PackageUtilsService.isPackagePublished)
-					.groupBy(function(value) {return value.member.key;})
-					.map(function(packages, memberKey) {
-						return {
-							member: MemberService.membersByKey[memberKey], 
-							packages: packages};})
-					.value();
+    $scope.releasePackagesByMember = _.chain(releasePackages)
+        .filter(PackageUtilsService.isPackagePublished)
+        .groupBy(function(value) {return value.member.key;})
+        .map(function(packages, memberKey) {
+            return {
+                member: MemberService.membersByKey[memberKey], 
+                packages: packages};})
+        .value();
+    if(Session.member != null)
+    {
+        var memberRelease = _.find($scope.releasePackagesByMember, function(item)
+        {
+            if(Session.member.key != null && Session.member.key != 'undefined' && Session.member.key != "IHTSDO")
+            {
+                if(item.member.key == Session.member.key)
+                {
+                    return 1;
+                }
+            }
+            else{return 0};
+        });
+        _.each($scope.releasePackagesByMember, function(item)
+        {
+            if(item.member.key == "IHTSDO" && memberRelease != null && memberRelease != 'undefined'){
+                _.each(memberRelease.packages, function(releasePackage){
+                    item.packages.push(releasePackage);
+                });
+            }
+        });
+    }
 }]);
