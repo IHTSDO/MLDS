7ced6eecf9def8600f4bc2fd9631e41dffda492a
Peter G. Williams <pwi@ihtsdo.org>
2015-03-16 15:26:28 +0000

    MLDS-899 Billing Address should now default for Member Countries who are using MLDS, and copy transparently so impossible to fix validation errors no longer encountered.

--- src/main/webapp/scripts/services/CountryService.js
+++ src/main/webapp/scripts/services/CountryService.js
@@ -10,14 +10,21 @@ angular.module('MLDS')
 		
 		service.countries = [];
 		service.countriesByIsoCode2 = {};
+		service.countriesUsingMLDS = [];
 		service.ready = countriesListQ;
 		
 		countriesListQ.then(function(countries){
 			// append to countries list
+			var tempCountriesUsingMLDS = [];
 			_.each(countries,function(country) {
 				country.commonName = $translate.instant('global.country.'+country.isoCode2);
+				//If this country is managed by someone other than IHTSDO then we know they're using MLDS themselves.
+				if (country.member.key != "IHTSDO") {
+					tempCountriesUsingMLDS.push(country.isoCode2);
+				}
 			});
 			Array.prototype.push.apply(service.countries,countries);
+			Array.prototype.push.apply(service.countriesUsingMLDS,tempCountriesUsingMLDS);
 			
 			service.countries.sort(function(a, b) {
 				var x = a.commonName.toLowerCase();
