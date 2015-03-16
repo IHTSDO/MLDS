edfb8dbad8a5902df95cf55191b365948d7e11f7
Peter G. Williams <pwi@ihtsdo.org>
2015-03-16 10:05:05 +0000

    MLDS-913 Prevent current country from being reset if user is redirected to a URL with no country specified.

--- src/main/webapp/scripts/controllers/MemberLandingPageController.js
+++ src/main/webapp/scripts/controllers/MemberLandingPageController.js
@@ -5,14 +5,18 @@ angular.module('MLDS').controller('MemberLandingPageController',
 		function ($scope, $rootScope, Session, $log, $routeParams, MemberService, $location, $translate, CountryService) {
 			var memberKey = $routeParams.memberKey;
 			var langISO = $location.search()['lang'];
-			console.log(memberKey + " with language " + langISO);
+			//console.log(memberKey + " with language " + langISO);
 			$translate.use(langISO);
 
 			MemberService.ready.then(function() {
 				var member = MemberService.membersByKey[memberKey];
 				// Introduce a new memberLanding variable rather than reuse Session.member as no
-				// authority is conferred by hitting this public page
-				$rootScope.memberLanding = member;
+				// authority is conferred by hitting this public page.
+				
+				//Do not reset the memberLanding if it has been set previously
+				if (member != null) {
+					$rootScope.memberLanding = member;
+				}
 			});
 
 			var setLandingText = function () {
@@ -23,7 +27,6 @@ angular.module('MLDS').controller('MemberLandingPageController',
 					CountryService.ready.then(function() {
 						var country = CountryService.countriesByIsoCode2[memberKey];
 						if (country != null) {
-							var countryCommonName = country.commonName;
 							$scope.landingText = 	$translate.instant('views.landingPage.member.purpose1') +
 													$translate.instant('global.country.'+ $rootScope.memberLanding.key) +
 													$translate.instant('views.landingPage.member.purpose2') +
