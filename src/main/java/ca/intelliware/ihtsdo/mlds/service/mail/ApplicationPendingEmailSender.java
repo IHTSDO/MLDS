801ff3604edebf6ff2cdbab7c1c47417f1a5bf3e
David Jones <david@dgjones.info>
2015-10-20 14:58:54 -0400

    MLDS-918 Update application pending variables and trigger

--- src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/ApplicationPendingEmailSender.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/ApplicationPendingEmailSender.java
@@ -7,12 +7,18 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import ca.intelliware.ihtsdo.mlds.domain.User;
-
+import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 
+import ca.intelliware.ihtsdo.mlds.domain.Application;
+import ca.intelliware.ihtsdo.mlds.domain.Member;
+
 /*
- * Send out email notification to the relevant Member staff when an affiliate has applied in their space.
+ * Send email notification to the relevant Member staff when an affiliate has applied in their space.
+ * 
+ * Use a shared email mailing list rather than to individual MLDS users as staff/admin are not registered
+ * directly in the MLDS system, so we instead use an IHTSDO hosted mailing list email address for the
+ * member organization.
  * 
  * https://jira.ihtsdotools.org/browse/MLDS-918
  */
@@ -23,19 +29,21 @@ public class ApplicationPendingEmailSender {
 	@Resource TemplateEvaluator templateEvaluator;
 	@Resource ClientLinkBuilder clientLinkBuilder;
 
-	public void sendApplicationPendingEmail(String email, String memberKey, long affiliateId, long applicationId) {
-		//FIXME shared account - which locale?
-		final Locale locale = Locale.forLanguageTag("en");
+	public void sendApplicationPendingEmail(String mailingListEmail, Application application) {
+		final Locale locale = Locale.ENGLISH;
+		Member member = application.getMember();
 		Map<String, Object> variables = Maps.newHashMap();
-		variables.put(EmailVariables.AFFILIATE_ID, Long.toString(affiliateId));
-		variables.put(EmailVariables.APPLICATION_ID, Long.toString(applicationId));
-		variables.put(EmailVariables.USER, email);
-		variables.put(EmailVariables.MEMBERKEY, memberKey);
-		variables.put(EmailVariables.VIEW_APPLICATION_URL, clientLinkBuilder.buildViewApplication(applicationId));
-		String content = templateEvaluator.evaluateTemplate("applicationApprovedEmail", locale, variables);
+		variables.put(EmailVariables.APPLICATION_ID, Long.toString(application.getApplicationId()));
+		variables.put(EmailVariables.APPLICATION_MEMBER, memberDescription(member));
+		variables.put(EmailVariables.VIEW_APPLICATION_URL, clientLinkBuilder.buildViewApplication(application.getApplicationId()));
+		String content = templateEvaluator.evaluateTemplate("applicationPendingEmail", locale, variables);
 		String subject = templateEvaluator.getTitleFor("applicationApproved", locale);
 		
-		mailService.sendEmail(email, subject, content, false, true);
+		mailService.sendEmail(mailingListEmail, subject, content, false, true);
+	}
+
+	private String memberDescription(Member member) {
+		return Strings.isNullOrEmpty(member.getName()) ? member.getKey() : member.getName();
 	}
 
 }
