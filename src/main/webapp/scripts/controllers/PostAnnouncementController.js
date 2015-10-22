cf0d92d5529047d086d5bbb0dd2c03c335ec58b4
David Jones <david@dgjones.info>
2015-10-22 13:23:55 -0400

    MLDS-860 Disable form after post with option to start again

--- src/main/webapp/scripts/controllers/PostAnnouncementController.js
+++ src/main/webapp/scripts/controllers/PostAnnouncementController.js
@@ -6,27 +6,44 @@ mldsApp.controller('PostAnnouncementController', [
 		'$location',
 		'$modal',
 		'$timeout',
+		'$route',
 		'Session',
 		'AnnouncementsService',
 		'MemberService',
-		function($scope, $log, $location, $modal, $timeout, Session, AnnouncementsService, MemberService) {
+		function($scope, $log, $location, $modal, $timeout, $route, Session, AnnouncementsService, MemberService) {
 			
 			$scope.alerts = [];
 			$scope.announcement = {
 					subject: '',
 					body: '',
-					member: Session.member || {member: 'NONE'},
+					member: sessionMemberPopulated(),
 					additionalEmails: []
 			};
 			$scope.emailListString = Session.email || ''; 
-			$scope.members = Session.isAdmin() ? MemberService.members : $scope.announcement.member;
+			$scope.members = Session.isAdmin() ? 
+					MemberService.members : [ $scope.announcement.member ];
 
 			$scope.form = {};
 	    	$scope.form.attempted = false;
 	    	
+	    	$scope.submitting = false
+	    	$scope.completed = false;
+	    	
+	    	
+	    	function sessionMemberPopulated() {
+	    		var member = Session.member || {key: 'NONE'};
+	    		var populatedMember = MemberService.membersByKey[member.key] || member;
+	    		return populatedMember;
+	    	}
+	    	
 	    	$scope.postAnnouncement = function () {
+	    		$scope.form.attempted = true;
+	    		
 	    		if ($scope.form.$invalid) {
-	    			$scope.form.attempted = true;
+	    			return;
+	    		}
+	    		
+	    		if ($scope.completed) {
 	    			return;
 	    		}
 
@@ -39,6 +56,7 @@ mldsApp.controller('PostAnnouncementController', [
 	    		AnnouncementsService.post({}, $scope.announcement)
 	    			.$promise.then(function(result) {
 	    				$scope.submitting = false;
+	    				$scope.completed = true;
 	    				$timeout(function(){
 		    				$scope.alerts.push({type: 'success', msg: 'Announcement has been successfully posted.'});
 	    				});
@@ -47,5 +65,9 @@ mldsApp.controller('PostAnnouncementController', [
 					$scope.alerts.push({type: 'danger', msg: 'Network request failure, please try again later.'});
 					$scope.submitting = false;
 				});
+	        };
+	        
+	        $scope.newAnnouncement = function newAnnouncement() {
+	        	$route.reload();
 	        };
 		} ]);
