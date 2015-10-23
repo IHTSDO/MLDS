61b19c615980c4936f28fbb8eb4064aa6bd508db
David Jones <david@dgjones.info>
2015-10-23 15:46:40 -0400

    MLDS-860 switch to minimal template with all content coming from announcement page

--- src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/AnnouncementEmailSender.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/service/mail/AnnouncementEmailSender.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Maps;
 
 import ca.intelliware.ihtsdo.mlds.domain.Member;
-import ca.intelliware.ihtsdo.mlds.domain.User;
 
 /*
  * Send email announcement to the relevant Member affiliates.
@@ -25,29 +23,12 @@ public class AnnouncementEmailSender {
 	@Resource TemplateEvaluator templateEvaluator;
 	@Resource ClientLinkBuilder clientLinkBuilder;
 
-	public void sendAnnouncementEmail(User user, Member member, String title, String message) {
-		String fullName = user.getFirstName() + ' ' + user.getLastName();
-		final Locale locale = Locale.forLanguageTag(user.getLangKey());
-		sendAnnouncementEmail(user.getEmail(), fullName, locale, member, title, message);
-	}
-
-	public void sendAnnouncementEmail(String email, Member member, String title, String message) {
-		sendAnnouncementEmail(email, "", Locale.ENGLISH, member, title, message);
-	}
-
-	public void sendAnnouncementEmail(String email, String fullName, Locale locale, Member member, String title, String message) {
-		String subject = title; 
-		if (StringUtils.isBlank(subject)) {
-			subject = templateEvaluator.getTitleFor("announcement", locale);
-		}
-		
+	public void sendAnnouncementEmail(String email, Member member, String subject, String message) {
 		Map<String, Object> variables = Maps.newHashMap();
-		variables.put(EmailVariables.ANNOUNCEMENT_NAME, fullName);
-		variables.put(EmailVariables.ANNOUNCEMENT_MESSAGE, message);
-		variables.put(EmailVariables.ANNOUNCEMENT_TITLE, subject);
-		variables.put(EmailVariables.LOGIN_URL, clientLinkBuilder.buildLoginLink());
+		variables.put(EmailVariables.BLANK_TITLE, subject);
+		variables.put(EmailVariables.BLANK_BODY, message);
 		
-		String content = templateEvaluator.evaluateTemplate("announcementEmail", locale, variables);
+		String content = templateEvaluator.evaluateTemplate("blankEmail", Locale.ENGLISH, variables);
 		
 		mailService.sendEmail(email, subject, content, false, true);
 	}
