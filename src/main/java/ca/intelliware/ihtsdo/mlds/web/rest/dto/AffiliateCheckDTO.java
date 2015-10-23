2cf1328566f4a06902dc189f9c16ac463f1b815f
David Jones <david@dgjones.info>
2015-10-23 10:36:54 -0400

    MLDS-917 wrap error message in response within an error object

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/dto/AffiliateCheckDTO.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/dto/AffiliateCheckDTO.java
@@ -1,9 +1,13 @@
 package ca.intelliware.ihtsdo.mlds.web.rest.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class AffiliateCheckDTO {
 
 	private Boolean matched;
-	private String error;
+	private ErrorDetails error;
 	
 	public Boolean getMatched() {
 		return matched;
@@ -11,10 +15,29 @@ public class AffiliateCheckDTO {
 	public void setMatched(Boolean matched) {
 		this.matched = matched;
 	}
-	public String getError() {
+	public ErrorDetails getError() {
 		return error;
 	}
-	public void setError(String error) {
+	public void setError(ErrorDetails error) {
 		this.error = error;
+	}
+	
+	public void setErrorMessage(String message) {
+		if (error == null) {
+			error = new ErrorDetails();
+		}
+		error.setMessage(message);
+	}
+	
+	public static class ErrorDetails {
+		private String message;
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
 	}
 }
