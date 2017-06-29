1a2996b1ca04ac2d9315067627d7495aeaabfb58
Peter Groves Williams <petergwilliams@hotmail.com>
2017-06-29 17:02:06 +0100

    Add support for JPA entityManagerFactory

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/DatabaseConfiguration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/DatabaseConfiguration.java
@@ -2,6 +2,7 @@ package ca.intelliware.ihtsdo.mlds.config;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 
@@ -9,14 +10,19 @@ import liquibase.integration.spring.SpringLiquibase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -32,21 +38,29 @@ public class DatabaseConfiguration implements EnvironmentAware {
 
     private RelaxedPropertyResolver propertyResolver;
 
-    private Environment environment;
+    private Environment env;
+    
+    private static DataSource dataSource;
+    
+    private static LocalContainerEntityManagerFactoryBean entityManagerFactory;
 
     @Override
     public void setEnvironment(Environment environment) {
-        this.environment = environment;
+        this.env = environment;
         this.propertyResolver = new RelaxedPropertyResolver(environment, "spring.datasource.");
     }
 
     @Bean
-    public DataSource dataSource() {
+    public synchronized DataSource dataSource() {
+    	if (dataSource != null) {
+    		log.debug("Skipping Datasource configuration - already configured.");
+    		return dataSource;
+    	}
         log.debug("Configuring Datasource");
         if (propertyResolver.getProperty("url") == null && propertyResolver.getProperty("databaseName") == null) {
             log.error("Your database connection pool configuration is incorrect! The application" +
                     "cannot start. Please check your Spring profile, current profiles are: {}",
-                    Arrays.toString(environment.getActiveProfiles()));
+                    Arrays.toString(env.getActiveProfiles()));
 
             throw new ApplicationContextException("Database connection pool is not configured correctly");
         }
@@ -60,7 +74,9 @@ public class DatabaseConfiguration implements EnvironmentAware {
         }
         config.addDataSourceProperty("user", propertyResolver.getProperty("username"));
         config.addDataSourceProperty("password", propertyResolver.getProperty("password"));
-        return new HikariDataSource(config);
+        dataSource = new HikariDataSource(config);
+        log.debug("Datasource configuration complete");
+        return dataSource;
     }
 
     @Bean(name = {"org.springframework.boot.autoconfigure.AutoConfigurationUtils.basePackages"})
@@ -83,5 +99,71 @@ public class DatabaseConfiguration implements EnvironmentAware {
         liquibase.setContexts("development, production");
         return liquibase;
     }
+	
+	//See http://blog.netgloo.com/2014/10/06/spring-boot-data-access-with-jpa-hibernate-and-mysql/
+	
+	/**
+	 * Declare the JPA entity manager factory.
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		if (entityManagerFactory != null) {
+    		log.debug("Skipping entityManagerFactory configuration - already configured.");
+			return entityManagerFactory;
+		}
+		log.debug("Configuring entityManagerFactory");
+		entityManagerFactory =  new LocalContainerEntityManagerFactoryBean();
+		
+		entityManagerFactory.setDataSource(dataSource());
+		
+		// Classpath scanning of @Component, @Service, etc annotated class
+		/*entityManagerFactory.setPackagesToScan(
+			env.getProperty("entitymanager.packagesToScan")); */
+		
+		// Vendor adapter
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+		
+		// Hibernate properties
+		Properties additionalProperties = new Properties();
+		additionalProperties.put(
+			"hibernate.dialect", 
+			env.getProperty("hibernate.dialect"));
+		additionalProperties.put(
+			"hibernate.show_sql", 
+			env.getProperty("hibernate.show_sql"));
+		additionalProperties.put(
+			"hibernate.hbm2ddl.auto", 
+			env.getProperty("hibernate.hbm2ddl.auto"));
+		entityManagerFactory.setJpaProperties(additionalProperties);
+		
+		return entityManagerFactory;
+	}
+
+	/**
+	 * Declare the transaction manager.
+	 */
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		log.debug("Configuring transactionManager");
+		JpaTransactionManager transactionManager = 
+			new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(
+			entityManagerFactory().getObject());
+		return transactionManager;
+	}
+	
+	/**
+	 * PersistenceExceptionTranslationPostProcessor is a bean post processor
+	 * which adds an advisor to any bean annotated with Repository so that any
+	 * platform-specific exceptions are caught and then rethrown as one
+	 * Spring's unchecked data access exceptions (i.e. a subclass of 
+	 * DataAccessException).
+	 */
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
 }
 
