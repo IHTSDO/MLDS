f8ee6ea9f14352bf269655bc758e6e83d353016a
Peter Groves Williams <petergwilliams@hotmail.com>
2017-07-05 16:44:03 +0100

    Add debug to help work out why privacy filters are not being found on dev.  Fine locally

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,8 @@ import ca.intelliware.ihtsdo.mlds.web.rest.dto.MemberDTO;
 @Configuration
 @ConditionalOnClass(ObjectMapper.class)
 public class JacksonConfigurer {
+	
+    private final Logger logger = LoggerFactory.getLogger(JacksonConfigurer.class);
 
 	@Autowired
 	private ListableBeanFactory beanFactory;
@@ -40,17 +44,19 @@ public class JacksonConfigurer {
 	
 	@PostConstruct
 	public void init() {
+		logger.debug("Initialising Jackson Mappers");
 		Collection<ObjectMapper> mappers = BeanFactoryUtils
 				.beansOfTypeIncludingAncestors(this.beanFactory, ObjectMapper.class)
 				.values();
 		for (ObjectMapper mapper : mappers) {
+			logger.debug("Configuring Jackson mapper: {}", mapper.getTypeFactory().getClass().getName());
 			mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-			
 			registerFilters(mapper);
 		}
 	}
 
 	public void registerFilters(ObjectMapper mapper) {
+		logger.debug("Registering jackson privacy filters");
 		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
 		filterProvider.addFilter("affiliatePrivacyFilter", new InternalPrivacyFilter(Affiliate.PRIVATE_FIELDS) );
 		filterProvider.addFilter("memberDtoPrivacyFilter", new InternalPrivacyFilter(MemberDTO.PRIVATE_FIELDS) );
