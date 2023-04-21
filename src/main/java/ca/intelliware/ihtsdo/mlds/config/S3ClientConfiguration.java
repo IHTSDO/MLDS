b695eee19e137d295aa7ad014ad3ec654888dbca
Aravinth Aasai <aravinth.a@telliant.net>
2023-04-21 14:47:05 +0530

    MLDS-984-- Commented the AWS keys present in two files

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/S3ClientConfiguration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/S3ClientConfiguration.java
@@ -19,8 +19,8 @@ public class S3ClientConfiguration implements EnvironmentAware {
 
 	private static final String ENV_S3_API = "s3.api.";
 	private static final String PROP_OFFLINE_MODE = "offlineMode";
-	private static final String PROP_AWS_KEY = "default.aws.key";
-	private static final String PROP_AWS_PRIVATEKEY = "default.aws.privateKey";
+//	private static final String PROP_AWS_KEY = "default.aws.key";
+//	private static final String PROP_AWS_PRIVATEKEY = " ";
 
 	private final Logger log = LoggerFactory.getLogger(S3ClientConfiguration.class);
 
@@ -43,10 +43,10 @@ public class S3ClientConfiguration implements EnvironmentAware {
 		if (s3Offline) {
 			s3Client = new OfflineS3ClientImpl();
 		} else {
-			String awsKey = propertyResolver.getProperty(PROP_AWS_KEY);
-			String awsPrivateKey = propertyResolver.getProperty(PROP_AWS_PRIVATEKEY);
-			BasicAWSCredentials credentials = new BasicAWSCredentials(awsKey, awsPrivateKey);
-			s3Client = new S3ClientImpl(credentials);
+//			String awsKey = propertyResolver.getProperty(PROP_AWS_KEY);
+//			String awsPrivateKey = propertyResolver.getProperty(PROP_AWS_PRIVATEKEY);
+//			BasicAWSCredentials credentials = new BasicAWSCredentials(awsKey, awsPrivateKey);
+//			s3Client = new S3ClientImpl(credentials);
 		}
 
 		log.debug("Done:Configuring S3 Client");
