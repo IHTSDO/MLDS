2b838fad37e03489a5863b03499ea9995265da2e
jaykamal <130353187+jaykamal@users.noreply.github.com>
2023-07-28 16:25:47 +0530

    MLDS-986 Code review fixes commented by Jim

--- src/test/java/ca/intelliware/ihtsdo/mlds/design/LiqubaseVerification.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/design/LiqubaseVerification.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class LiqubaseVerification {
 	@Test
 	public void verifyThatWeAreNotUsingTheDefaultJHGeneratedMigration() throws Exception {
-		boolean jhipsterGeneratedChangelogExists = new File("src/main/resources/config/liquibase/changelog/db-changelog-002.xml").exists();
+		boolean jhipsterGeneratedChangelogExists = new File("src/main/resources/config/liquibase/changelog/db-changelog-003.xml").exists();
 		assertFalse("we don't use the auto-generated migrations, except as a starting point for editing",jhipsterGeneratedChangelogExists);
 	}
 }
