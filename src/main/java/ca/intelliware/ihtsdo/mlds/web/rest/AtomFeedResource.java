90ea7df656fc8f101a0139ce57e2dc41926eebc4
jaykamal <130353187+jaykamal@users.noreply.github.com>
2023-10-10 12:22:02 +0530

    MLDS-1021 Syndication feed refinement

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/AtomFeedResource.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/AtomFeedResource.java
@@ -1,70 +1,25 @@
 package ca.intelliware.ihtsdo.mlds.web.rest;
 
-import ca.intelliware.ihtsdo.mlds.repository.ReleaseVersionRepository;
-import ca.intelliware.ihtsdo.mlds.security.AuthoritiesConstants;
 import ca.intelliware.ihtsdo.mlds.service.AtomFeedService;
-import com.codahale.metrics.annotation.Timed;
-import com.rometools.rome.feed.atom.Feed;
-import com.rometools.rome.feed.module.Module;
-import com.rometools.rome.feed.module.ModuleImpl;
-import com.rometools.rome.feed.synd.SyndEntry;
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.feed.synd.SyndFeedImpl;
-import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.SyndFeedOutput;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.Namespace;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
-import org.jsoup.Jsoup;
-import org.jsoup.parser.Parser;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import javax.annotation.security.RolesAllowed;
-import javax.transaction.Transactional;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(value = Routes.FEED_URL,
     method= RequestMethod.GET,
     produces = MediaType.APPLICATION_ATOM_XML_VALUE)
-@Transactional
 public class AtomFeedResource {
 
-    @Resource
-    ReleaseVersionRepository releaseVersionRepository;
-
-    @Resource
-    Environment environment;
-
+    @Autowired
+    private AtomFeedService atomFeedService;
 
     @GetMapping(produces = MediaType.APPLICATION_ATOM_XML_VALUE)
-    public ModelAndView getReleaseFeed() {
-
-
-        SyndFeed feed = new SyndFeedImpl();
-        feed.setFeedType("atom_1.0");
-        List<SyndEntry> entries = new ArrayList<>();
-        feed.setEntries(entries);
-        return new ModelAndView(new AtomFeedService(releaseVersionRepository, environment), "feed", feed);
-     }
-
+    public String getAtomFeed() {
+        return atomFeedService.generateAtomFeed();
+    }
 }
