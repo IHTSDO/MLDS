1e4f89ae510df3cf32c34f03eb2ae978db1aa52f
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-03-15 20:52:54 +0530

    MLDS-1025 build issues fixes

--- src/main/java/ca/intelliware/ihtsdo/mlds/search/StandingStateFieldBridge.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/search/StandingStateFieldBridge.java
@@ -1,8 +1,8 @@
-package ca.intelliware.ihtsdo.mlds.search;
-
-public class StandingStateFieldBridge extends TranslatedEnumFieldBridge {
-	@Override
-	protected String getBundleKeyPrefix() {
-		return "affiliate.standingState.";
-	}
-}
+//package ca.intelliware.ihtsdo.mlds.search;
+//
+//public class StandingStateFieldBridge extends TranslatedEnumFieldBridge {
+//	@Override
+//	protected String getBundleKeyPrefix() {
+//		return "affiliate.standingState.";
+//	}
+//}
