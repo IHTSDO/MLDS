412ae15f136c8d53b8be4bd6760a44d18e41ebcc
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-03-21 19:18:16 +0530

    MLDS-1025 Affiliate listing issue fixes

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/JacksonConfigurer.java
@@ -1,16 +1,8 @@
 package ca.intelliware.ihtsdo.mlds.config;
 
-import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
-import ca.intelliware.ihtsdo.mlds.domain.json.InternalPrivacyFilter;
-import ca.intelliware.ihtsdo.mlds.domain.json.MLDSJacksonModule;
-import ca.intelliware.ihtsdo.mlds.repository.MemberRepository;
-import ca.intelliware.ihtsdo.mlds.security.ihtsdo.CurrentSecurityContext;
-import ca.intelliware.ihtsdo.mlds.web.rest.dto.MemberDTO;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import java.util.Collection;
+
+
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,67 +10,58 @@ import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.core.annotation.Order;
 
 
-import java.util.Collection;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+import ca.intelliware.ihtsdo.mlds.domain.Affiliate;
+import ca.intelliware.ihtsdo.mlds.domain.json.InternalPrivacyFilter;
+import ca.intelliware.ihtsdo.mlds.web.rest.dto.MemberDTO;
 
 @Configuration
 @ConditionalOnClass(ObjectMapper.class)
 @DependsOn("passwordEncoder")
 public class JacksonConfigurer {
 
-	private final Logger logger = LoggerFactory.getLogger(JacksonConfigurer.class);
+    private final Logger logger = LoggerFactory.getLogger(JacksonConfigurer.class);
 
-	@Autowired
-	private ListableBeanFactory beanFactory;
+    @Autowired
+    private ListableBeanFactory beanFactory;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
 
-//	@Autowired
-//	private ObjectMapper objectMapper;
+    @PostConstruct
+    public void init() {
+        logger.debug("Initialising Jackson Mappers using {}", beanFactory.getClass().getName());
 
-	@Bean
-	public Module mldsModule(final MemberRepository memberRepository, CurrentSecurityContext securityContext) {
-		logger.debug("Creating MLDS Jackson Module bean");
-		SimpleModule mldsModule = new MLDSJacksonModule(memberRepository, securityContext);
-		return mldsModule;
-	}
+        Collection<ObjectMapper> mappers = BeanFactoryUtils
+            .beansOfTypeIncludingAncestors(beanFactory, ObjectMapper.class,true, true)
+            .values();
+        int mappersConfigured = 0;
+        for (ObjectMapper mapper : mappers) {
+            logger.debug("Configuring Jackson mapper: {} with type factory {}", mapper, mapper.getTypeFactory().getClass().getName());
+            mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+            registerFilters(mapper);
+            mappersConfigured++;
+        }
 
-	@PostConstruct
-    @Order(Integer.MAX_VALUE)
-	public void init() {
-		logger.debug("Initialising Jackson Mappers using {}", beanFactory.getClass().getName());
+        if (mappersConfigured == 0) {
+            logger.error("*** NO MAPPERS FOUND TO CONFIGURE! ***");
+        }
+    }
 
-		Collection<ObjectMapper> mappersValue = BeanFactoryUtils
-				.beansOfTypeIncludingAncestors(beanFactory, ObjectMapper.class,true, true)
-				.values();
-        logger.info("mapper values {}", mappersValue.stream().count());
-        Map<String, ObjectMapper> mappers = beanFactory.getBeansOfType(ObjectMapper.class);
-		int mappersConfigured = 0;
-        logger.info("mapper values {}", mappers.values());
-		for (ObjectMapper mapper : mappers.values()) {
-			logger.debug("Configuring Jackson mapper: {} with type factory {}", mapper.toString(), mapper.getTypeFactory().getClass().getName());
-			mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-			registerFilters(mapper);
-			mappersConfigured++;
-		}
-
-		if (mappersConfigured == 0) {
-			logger.error("*** NO MAPPERS FOUND TO CONFIGURE! ***");
-		}
-	}
-
-	public void registerFilters(ObjectMapper mapper) {
-		logger.debug("Registering jackson privacy filters");
-		SimpleFilterProvider filterProvider = new SimpleFilterProvider();
-		filterProvider.addFilter("affiliatePrivacyFilter", new InternalPrivacyFilter(Affiliate.PRIVATE_FIELDS) );
-		filterProvider.addFilter("memberDtoPrivacyFilter", new InternalPrivacyFilter(MemberDTO.PRIVATE_FIELDS) );
-		mapper.setFilters(filterProvider);
-	}
+    public void registerFilters(ObjectMapper mapper) {
+        logger.debug("Registering jackson privacy filters");
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("affiliatePrivacyFilter", new InternalPrivacyFilter(Affiliate.PRIVATE_FIELDS) );
+        filterProvider.addFilter("memberDtoPrivacyFilter", new InternalPrivacyFilter(MemberDTO.PRIVATE_FIELDS) );
+        mapper.setFilterProvider(filterProvider);
+    }
 
 }
