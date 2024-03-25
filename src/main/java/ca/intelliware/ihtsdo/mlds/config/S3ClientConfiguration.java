1b6110ae70eaf570eb5498eba715a4d53332eab9
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-03-25 15:23:05 +0530

    MLDS-1024 S3 Region issue fixes - declared in property

--- src/main/java/ca/intelliware/ihtsdo/mlds/config/S3ClientConfiguration.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/config/S3ClientConfiguration.java
@@ -7,19 +7,23 @@ import org.ihtsdo.otf.dao.s3.S3Client;
 import org.ihtsdo.otf.dao.s3.S3ClientImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
-import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain;
+import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class S3ClientConfiguration implements EnvironmentAware {
 
     private static final String ENV_S3_API = "s3.api.";
     private static final String PROP_OFFLINE_MODE = "offline-mode";
+
+    @Value("${aws.region}")
+    private String awsRegion;
 
     private final Logger log = LoggerFactory.getLogger(S3ClientConfiguration.class);
 
@@ -46,7 +50,7 @@ public class S3ClientConfiguration implements EnvironmentAware {
         } else {
             s3Client = new S3ClientImpl(software.amazon.awssdk.services.s3.S3Client.builder()
                 .credentialsProvider(InstanceProfileCredentialsProvider.create())
-                .region(DefaultAwsRegionProviderChain.builder().build().getRegion())
+                .region(Region.of(awsRegion))
                 .build());
             log.debug("s3Client:", s3Client);
         }
