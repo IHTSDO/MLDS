389b9fa42d6a0136f61a4bcc2dfb63b2d8d09625
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-04-12 20:26:07 +0530

    MLDS-1025 Address The Code review Comments given by Darryn
    Test files implemented for the hibernate search indexer.

---
+++ src/test/java/ca/intelliware/ihtsdo/mlds/developer/HibernateSearchIndexer.java
@@ -0,0 +1,28 @@
+package ca.intelliware.ihtsdo.mlds.developer;
+
+import jakarta.annotation.Resource;
+import jakarta.persistence.EntityManager;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
+public class HibernateSearchIndexer {
+    @Resource
+    EntityManager entityManager;
+
+    @Test
+    public void recreateIndex() throws Exception {
+        SearchSession fullTextEntityManager = Search.session(entityManager);
+
+        //fullTextEntityManager.createIndexer().startAndWait();
+        fullTextEntityManager.toString();
+    }
+
+}
