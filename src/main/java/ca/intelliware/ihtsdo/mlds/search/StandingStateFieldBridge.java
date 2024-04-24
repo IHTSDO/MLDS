4eb11904fea55459ce8f42fecdd07595231e0bfd
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-04-24 18:53:08 +0530

    MLDS-1025 Address The Code review Comments given by Darryn

--- src/main/java/ca/intelliware/ihtsdo/mlds/search/StandingStateFieldBridge.java
+++
@@ -1,8 +0,0 @@
-//package ca.intelliware.ihtsdo.mlds.search;
-//
-//public class StandingStateFieldBridge extends TranslatedEnumFieldBridge {
-//	@Override
-//	protected String getBundleKeyPrefix() {
-//		return "affiliate.standingState.";
-//	}
-//}
