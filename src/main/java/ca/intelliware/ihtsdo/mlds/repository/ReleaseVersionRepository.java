5a016ae3ff90e6ccf6e2b68ad25bc3f188c3d4ac
Aravinth <109154990+AravinthAasaithambi@users.noreply.github.com>
2024-07-10 19:30:19 +0530

    MLDS-1051 Syndication dependencies should be highlighted when about to be Removed
    Sonar Lint Fix

--- src/main/java/ca/intelliware/ihtsdo/mlds/repository/ReleaseVersionRepository.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/repository/ReleaseVersionRepository.java
@@ -3,7 +3,6 @@ package ca.intelliware.ihtsdo.mlds.repository;
 
 import ca.intelliware.ihtsdo.mlds.domain.ReleaseVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
