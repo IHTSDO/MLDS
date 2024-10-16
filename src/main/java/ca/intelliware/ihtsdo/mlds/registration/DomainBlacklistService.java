ddf7ee9d8a3170fb63a2db235c4f252be4b5308d
Aravinth <109154990+AravinthAasaithambi@users.noreply.github.com>
2024-10-16 18:27:33 +0530

    MLDS - 1142
    Column Name consistent for Email Domain Blocklist

--- src/main/java/ca/intelliware/ihtsdo/mlds/registration/DomainBlacklistService.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/registration/DomainBlacklistService.java
@@ -11,25 +11,25 @@ import java.util.List;
 public class DomainBlacklistService {
 	@Resource
 	DomainBlacklistRespository domainBlacklistRespository;
-	
+
 	public boolean isDomainBlacklisted(String email) {
 		String requestEmailDomain = extractDomain(email);
-				
-		List<DomainBlacklist> result = domainBlacklistRespository.findByDomainname(requestEmailDomain);
-		
+
+		List<DomainBlacklist> result = domainBlacklistRespository.findByDomainName(requestEmailDomain);
+
 		if (result.size() > 0) {
 			return true;
 		}
-				
+
 		return false;
 	}
 
 	private String extractDomain(String email) {
 		String[] result;
 		String delimiter = "@";
-		
+
 		result = email.split(delimiter);
-		
+
 		return result[1];
 	}
 }
