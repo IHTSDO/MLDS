17e77351e3249fae888cdc2c4d6dea847bc85d93
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-10-17 18:36:19 +0530

    MLDS-1145 Table name consistent for t_authority

--- src/main/java/ca/intelliware/ihtsdo/mlds/domain/Authority.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/domain/Authority.java
@@ -15,11 +15,11 @@ import java.io.Serializable;
  * An authority (a security role) used by Spring Security.
  */
 @Entity
-@Table(name = "T_AUTHORITY")
+@Table(name = "authority")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Authority implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@NotNull
     @Size(min = 0, max = 50)
     @Id
