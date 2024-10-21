cbe0f3a793b9c219d8c69bfb986e67d450f14da6
jaykamal <130353187+jaykamal@users.noreply.github.com>
2024-10-21 19:50:05 +0530

    MLDS-1153 Git Repository : Document Update - DDL Update

--- src/main/ad-hoc/minimal-dev-db.sql
+++ src/main/ad-hoc/minimal-dev-db.sql
@@ -1,15 +1,15 @@
 use mlds;
 
--- Inserting data into t_authority table
-INSERT INTO t_authority (name) VALUES ('ROLE_ADMIN');
-INSERT INTO t_authority (name) VALUES ('ROLE_USER');
-INSERT INTO t_authority (name) VALUES ('ROLE_STAFF');
-INSERT INTO t_authority (name) VALUES ('ROLE_STAFF_SE');
-INSERT INTO t_authority (name) VALUES ('ROLE_STAFF_IHTSDO');
+-- Inserting data into authority table
+INSERT INTO authority (name) VALUES ('ROLE_ADMIN');
+INSERT INTO authority (name) VALUES ('ROLE_USER');
+INSERT INTO authority (name) VALUES ('ROLE_STAFF');
+INSERT INTO authority (name) VALUES ('ROLE_STAFF_SE');
+INSERT INTO authority (name) VALUES ('ROLE_STAFF_IHTSDO');
 
 
--- Inserting data into t_user table
-INSERT INTO t_user (login, user_id, password, first_name, last_name, email, activated, lang_key, activation_key, created_by, created_date, last_modified_by, last_modified_date)
+-- Inserting data into user table
+INSERT INTO user (login, user_id, password, first_name, last_name, email, activated, lang_key, activation_key, created_by, created_date, last_modified_by, last_modified_date)
 VALUES
     ('system', 1, '572d3b834f32347527d749bc1a41042c920682fc430febd380b4b6a0134f314fd381ce11c9a05abe', NULL, 'System', NULL, true, 'en', NULL, 'system', '2014-06-24 12:29:08', NULL, NULL),
     ('anonymousUser', 2, '4f54479f8290dfd503b72a654faf5d70593eab443993d87a79e14e5f7cda3eb7988423aa99090c9b', 'Anonymous', 'User', NULL, true, 'en', NULL, 'system', '2014-06-24 12:29:08', NULL, NULL),
@@ -19,8 +19,8 @@ VALUES
     ('sweden', 6, 'e6b4dc6acaa889021ab202e417245c0880c66b254ad5758a298d310d15d38f1385bc0d82fc6f5e62', NULL, NULL, NULL, true, NULL, NULL, 'system', '2014-07-25 10:27:30', NULL, NULL);
 
 
--- Inserting data into t_user_authority table
-INSERT INTO t_user_authority (user_id, name)
+-- Inserting data into user_authority table
+INSERT INTO user_authority (user_id, name)
 VALUES
     (1, 'ROLE_ADMIN'),
     (1, 'ROLE_USER'),
