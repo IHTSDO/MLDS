2568b26fe9cad2aebfa1124fb586789edfa15ec9
jaykamal <jayakamala.n@telliant.net>
2025-02-19 18:34:57 +0530

    MLDS-1163 Unit test method to update member feed url

--- src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/MemberResourceTest.java
+++ src/test/java/ca/intelliware/ihtsdo/mlds/web/rest/MemberResourceTest.java
@@ -30,33 +30,33 @@ public class MemberResourceTest {
 	@Mock private MemberRepository memberRepository;
 	@Mock private FileRepository fileRepository;
 	@Mock private SessionService sessionService;
-	
+
 	private MockMvc restUserMockMvc;
-	
+
 	@Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
         MemberResource memberResource = new MemberResource();
-        
+
         memberResource.memberRepository = memberRepository;
         memberResource.fileRepository = fileRepository;
         memberResource.sessionService = sessionService;
-        
+
         this.restUserMockMvc = MockMvcBuilders
         		.standaloneSetup(memberResource)
         		.setMessageConverters(new MockMvcJacksonTestSupport().getConfiguredMessageConverters())
         		.build();
-        
+
         Mockito.when(memberRepository.findAll()).thenReturn(Arrays.asList(new Member("SE", 1), new Member("IHTSDO", 2)));
 	}
-	
+
     @Test
     public void getMembersShouldIncludeAllMembers() throws Exception {
     	restUserMockMvc.perform(get(Routes.MEMBERS)
     			.accept(MediaType.APPLICATION_JSON_UTF8))
     			.andExpect(status().isOk())
     			.andExpect(content().string(Matchers.allOf(Matchers.containsString("SE"), Matchers.containsString("IHTSDO"))));
-    	
+
     }
 
 	@Test
@@ -66,22 +66,22 @@ public class MemberResourceTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(Matchers.containsString("\"memberId\":1,\"key\":\"SE\"")));
 	}
-	
+
 	@Test
 	public void postMembersNotificationsShouldUpdateStaffNotifications() throws Exception {
 		Member member = new Member("SE", 1L);
 		Mockito.when(memberRepository.findOneByKey("SE")).thenReturn(member);
-		
+
         restUserMockMvc.perform(put(Routes.MEMBER_NOTIFICATIONS, "SE")
         		.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.content("{ \"staffNotificationEmail\": \"staff@test.com\" }")
                 .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
-        
+
         Mockito.verify(memberRepository).save(member);
         assertThat(member.getStaffNotificationEmail(), equalTo("staff@test.com"));
 	}
-	
+
 	@Test
 	public void updateMemberShouldFailForUnknownMember() throws Exception {
         restUserMockMvc.perform(put(Routes.MEMBER, "ZZ")
@@ -89,7 +89,7 @@ public class MemberResourceTest {
 				.content("{ \"staffNotificationEmail\": \"new@test.com\" }")
                 .accept(MediaType.APPLICATION_JSON_UTF8))
         .andExpect(status().isNotFound());
-        
+
         Mockito.verify(memberRepository, never()).save(Mockito.any(Member.class));
 	}
 
@@ -99,18 +99,53 @@ public class MemberResourceTest {
 		member.setStaffNotificationEmail("old@test.com");
 		member.setPromotePackages(false);
 		Mockito.when(memberRepository.findOneByKey("SE")).thenReturn(member);
-		
+
         restUserMockMvc.perform(put(Routes.MEMBER, "SE")
         		.contentType(MediaType.APPLICATION_JSON_UTF8)
 				.content("{ \"promotePackages\": true, \"staffNotificationEmail\": \"new@test.com\" }")
                 .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isOk());
-        
+
         Mockito.verify(memberRepository).save(member);
-        
+
         assertThat(member.getStaffNotificationEmail(), equalTo("new@test.com"));
         assertThat(member.getPromotePackages(), equalTo(true));
 	}
-	
-	
+
+    @Test
+    public void updateMemberFeedURLShouldUpdateFields() throws Exception {
+        Member member = new Member("SE", 1L);
+        member.setContactEmail("oldemail@example.com");
+        member.setMemberOrgURL("http://old-url.com");
+        member.setMemberOrgName("Old Org");
+        Mockito.when(memberRepository.findOneByKey("SE")).thenReturn(member);
+
+        restUserMockMvc.perform(put(Routes.MEMBER_FEED_URL, "SE")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content( "{ \"contactEmail\": \"newemail@example.com\", " +
+                    "\"memberOrgURL\": \"http://new-url.com\", " +
+                    "\"memberOrgName\": \"New Org\" }")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        Mockito.verify(memberRepository).save(member);
+        assertThat(member.getContactEmail(), equalTo("newemail@example.com"));
+        assertThat(member.getMemberOrgURL(), equalTo("http://new-url.com"));
+        assertThat(member.getMemberOrgName(), equalTo("New Org"));
+    }
+
+    @Test
+    public void updateMemberFeedURLShouldReturnNotFoundForUnknownMember() throws Exception {
+        Mockito.when(memberRepository.findOneByKey("ZZ")).thenReturn(null);
+
+        restUserMockMvc.perform(put(Routes.MEMBER_FEED_URL, "ZZ")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content("{ \"contactEmail\": \"newemail@example.com\", " +
+                    "\"memberOrgURL\": \"http://new-url.com\", " +
+                    "\"memberOrgName\": \"New Org\" }")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+            .andExpect(status().isNotFound());
+        Mockito.verify(memberRepository, never()).save(Mockito.any(Member.class));
+    }
+
 }
