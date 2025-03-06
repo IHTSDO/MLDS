5991724452f51e1315ea37dc2e09c92b75312921
AravinthAasai <109154990+AravinthAasaithambi@users.noreply.github.com>
2025-03-06 15:41:22 +0530

    Add the ability to auto decline/deactivate an account if no activity

--- src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
+++ src/main/java/ca/intelliware/ihtsdo/mlds/web/rest/Routes.java
@@ -219,5 +219,8 @@ public class Routes {
 
     public static final String FEED_URL = "/api/feed";
 
+    public static final String MEMBER_AUTO_DEACTIVATION = "/api/members/{memberKey}";
+    public static final String POST_MEMBER_AUTO_DEACTIVATION = "/api/members/{memberKey}/autoDeactivation";
+
 
 }
